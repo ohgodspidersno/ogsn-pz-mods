d52095409a32c5d7e675c33d1f5e6d070e85a038
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-03 04:57:32 -0400

    Adds an optional patch that removes bean bowl, hot cuppa, and cold cuppa from the game (untested).

---
+++ VanillaFoodFixesOGSN/Contents/mods/0_RemoveOutdatedThings/media/lua/client/tweaks_removeOutDatedThings.lua
@@ -0,0 +1,7 @@
+-- if getActivatedMods():contains("ItemTweakerAPI") then
+require("ItemTweaker_Copy_ogsn");
+-- else return end
+
+TweakItem("Base.BeanBowl","OBSOLETE","true");
+TweakItem("Base.ColdCuppa","OBSOLETE","true");
+TweakItem("Base.Mugfull","OBSOLETE","true");
