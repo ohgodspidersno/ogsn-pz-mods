f842dae9fff41c23077dc76f94cba94ae8d9b1de
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-05 12:02:18 -0400

    Gives vanilla food fixes its own module name

--- VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_cookwithherbs.lua
+++ VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_cookwithherbs.lua
@@ -1,9 +1,9 @@
 require("ItemTweaker_Copy_ogsn");
 
 -- VANILLA ITEMS
-TweakItem("Base.Sugar","EvolvedRecipe","HotDrink:1;Evolved_CommonMallow:1;Evolved_LemonGrass:1;Evolved_BlackSage:1;Evolved_Ginseng:1;Evolved_Energizing:1;Evolved_Medicinal:1");
-TweakItem("Base.Milk","EvolvedRecipe","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2");
-TweakItem("Base.Honey","EvolvedRecipe","HotDrink:5;Evolved_CommonMallow:5;Evolved_LemonGrass:5;Evolved_BlackSage:5;Evolved_Ginseng:5;Evolved_Energizing:5;Evolved_Medicinal:5");
+TweakItem("Base.Sugar","EvolvedRecipe","HotDrink:1;VanillaFoodFixesOGSN.Evolved_CommonMallow:1;VanillaFoodFixesOGSN.Evolved_LemonGrass:1;VanillaFoodFixesOGSN.Evolved_BlackSage:1;VanillaFoodFixesOGSN.Evolved_Ginseng:1;VanillaFoodFixesOGSN.Evolved_Energizing:1;VanillaFoodFixesOGSN.Evolved_Medicinal:1");
+TweakItem("Base.Milk","EvolvedRecipe","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2");
+TweakItem("Base.Honey","EvolvedRecipe","HotDrink:5;VanillaFoodFixesOGSN.Evolved_CommonMallow:5;VanillaFoodFixesOGSN.Evolved_LemonGrass:5;VanillaFoodFixesOGSN.Evolved_BlackSage:5;VanillaFoodFixesOGSN.Evolved_Ginseng:5;VanillaFoodFixesOGSN.Evolved_Energizing:5;VanillaFoodFixesOGSN.Evolved_Medicinal:5");
 
 -- MEDICINAL HERBS
 TweakItem("Base.CommonMallow","HungerChange", "-2");
@@ -28,13 +28,13 @@ TweakItem("Base.Ginseng","EvolvedRecipe","Soup:1;Stew:1;Pie:1;Cake:1;Stir fry:1;
 
 -- FOOD HERBS
 TweakItem("Base.Rosehips","FoodType","Herbal");
-TweakItem("Base.Rosehips","EvolvedRecipe","Soup:3;Stew:3;Pie:3;Cake:3;Stir fry:3;Stir fry Griddle Pan:3;Roasted Vegetables:3;Burger:3;Salad:3;PastaPan:3;RicePan:3;PastaPot:3;RicePot:3;HotDrink:3;Evolved_CommonMallow:3;Evolved_LemonGrass:3;Evolved_BlackSage:3;Evolved_Ginseng:3;Evolved_Energizing:3;Evolved_Medicinal:3");
+TweakItem("Base.Rosehips","EvolvedRecipe","Soup:3;Stew:3;Pie:3;Cake:3;Stir fry:3;Stir fry Griddle Pan:3;Roasted Vegetables:3;Burger:3;Salad:3;PastaPan:3;RicePan:3;PastaPot:3;RicePot:3;HotDrink:3;VanillaFoodFixesOGSN.Evolved_CommonMallow:3;VanillaFoodFixesOGSN.Evolved_LemonGrass:3;VanillaFoodFixesOGSN.Evolved_BlackSage:3;VanillaFoodFixesOGSN.Evolved_Ginseng:3;VanillaFoodFixesOGSN.Evolved_Energizing:3;VanillaFoodFixesOGSN.Evolved_Medicinal:3");
 
 TweakItem("Base.GrapeLeaves","FoodType","Herbal")
-TweakItem("Base.GrapeLeaves","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2")
+TweakItem("Base.GrapeLeaves","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2")
 
 TweakItem("Base.Violets","FoodType","Herbal")
-TweakItem("Base.Violets","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Cake:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;FruitSalad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2")
+TweakItem("Base.Violets","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Cake:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;FruitSalad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2")
 
 -- COMPATIBILITY WITH OTHER FOOD MODS
 if getActivatedMods():contains("AAApoc") then
@@ -43,13 +43,13 @@ if getActivatedMods():contains("AAApoc") then
 else end
 
 if getActivatedMods():contains("SKLDairyModBeta") then
-    TweakItem("Base.MilkJar","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2");
-    TweakItem("Base.MilkWaterBottle","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2");
-    TweakItem("Base.MilkPopBottle","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2");
+    TweakItem("Base.MilkJar","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2");
+    TweakItem("Base.MilkWaterBottle","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2");
+    TweakItem("Base.MilkPopBottle","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2");
 else end
 
 if getActivatedMods():contains("SKLDairyModBeta") and getActivatedMods():contains("WarOnBeans") then
-    TweakItem("Base.MilkJar","EvolvedRecipe","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2;OatmealEvolved:2");
-    TweakItem("Base.MilkWaterBottle","EvolvedRecipe","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2;OatmealEvolved:2");
-    TweakItem("Base.MilkPopBottle","EvolvedRecipe","HotDrink:2;Evolved_CommonMallow:2;Evolved_LemonGrass:2;Evolved_BlackSage:2;Evolved_Ginseng:2;Evolved_Energizing:2;Evolved_Medicinal:2;OatmealEvolved:2");
+    TweakItem("Base.MilkJar","EvolvedRecipe","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2;VanillaFoodFixesOGSN.OatmealEvolved:2");
+    TweakItem("Base.MilkWaterBottle","EvolvedRecipe","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2;VanillaFoodFixesOGSN.OatmealEvolved:2");
+    TweakItem("Base.MilkPopBottle","EvolvedRecipe","HotDrink:2;VanillaFoodFixesOGSN.Evolved_CommonMallow:2;VanillaFoodFixesOGSN.Evolved_LemonGrass:2;VanillaFoodFixesOGSN.Evolved_BlackSage:2;VanillaFoodFixesOGSN.Evolved_Ginseng:2;VanillaFoodFixesOGSN.Evolved_Energizing:2;VanillaFoodFixesOGSN.Evolved_Medicinal:2;VanillaFoodFixesOGSN.OatmealEvolved:2");
 else end
