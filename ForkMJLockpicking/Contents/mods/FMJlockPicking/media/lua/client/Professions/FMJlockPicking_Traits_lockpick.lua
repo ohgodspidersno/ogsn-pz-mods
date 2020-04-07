0ac049532485135bb3861755d52e6190456dbdba
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-07 18:38:40 -0400

    Removes starting item sub-mod, instead makes bobbypins spawn if occupational items on spawn is also loaded

--- ForkMJLockpicking/Contents/mods/FMJlockPicking/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
+++ ForkMJLockpicking/Contents/mods/FMJlockPicking/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
@@ -8,13 +8,15 @@ local function initTraits()
 
 Events.OnGameBoot.Add(initTraits);
 
-Events.OnNewGame.Add(function(player, square)
-	if player:HasTrait("NimbleFingers") then
-    	player:getInventory():AddItem("FMJ.BobbyPin");
-    	player:getInventory():AddItem("FMJ.BobbyPin")
-    end
-	if player:HasTrait("NimbleFingers2") then
-    	player:getInventory():AddItem("FMJ.BobbyPin");
-    	player:getInventory():AddItem("FMJ.BobbyPin")
-	end
-end)
+if getActivatedMods():contains("oka_oiosb41") then
+		Events.OnNewGame.Add(function(player, square)
+			if player:HasTrait("NimbleFingers") then
+		    	player:getInventory():AddItem("FMJ.BobbyPin");
+		    	player:getInventory():AddItem("FMJ.BobbyPin")
+		    end
+			if player:HasTrait("NimbleFingers2") then
+		    	player:getInventory():AddItem("FMJ.BobbyPin");
+		    	player:getInventory():AddItem("FMJ.BobbyPin")
+			end
+		end)
+end
