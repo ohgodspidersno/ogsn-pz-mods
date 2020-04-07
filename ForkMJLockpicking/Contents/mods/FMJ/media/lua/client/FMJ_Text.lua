f2dd25c9074cdc51adc1d26227f7eb14f0052780
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-07 16:27:22 -0400

    Removes the original full FMJ mod folder

--- ForkMJLockpicking/Contents/mods/FMJ/media/lua/client/FMJ_Text.lua
+++
@@ -1,8 +0,0 @@
--- Contains all human readable text of the mod.
-FMJ_Text = {};
-
-FMJ_Text.lockLevels = { getText("UI_Text_locklevel_veryeasy"),getText("UI_Text_locklevel_easy"),getText("UI_Text_locklevel_medium"),getText("UI_Text_locklevel_hard"),getText("UI_Text_locklevel_veryhard"),getText("UI_Text_locklevel_willbreak") };
-FMJ_Text.brokenLockModal = getText("UI_Text_brokenLockModal");
-FMJ_Text.contextBreakDoorLock = getText("UI_Text_contextBreakDoorLock");
-FMJ_Text.contextPickDoorLock = getText("UI_Text_contextPickDoorLock");
-FMJ_Text.contextBreakWindowLock = getText("UI_Text_contextBreakWindowLock");
