405d9893d05710244545a1405b527fba9a4e51a6
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-10 23:30:05 -0400

    Changes all NimbleFingers to be nimblefingers all lowercase. Choosing lowercase instead of PascalCase because I want to match the image file names (wwhich steam won't recognize that I've changed, with disastrous results, as I've learned in my Rimworld modding.

--- LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
+++ LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
@@ -11,11 +11,11 @@ Events.OnGameBoot.Add(initTraits);
 if getActivatedMods():contains("oka_oiosb41") then
 	Events.OnNewGame.Add(
 		function(player, square)
-			if player:HasTrait("NimbleFingers") then
+			if player:HasTrait("nimblefingers") then
 	    	player:getInventory():AddItem("FMJ.BobbyPin");
 	    	player:getInventory():AddItem("FMJ.BobbyPin")
 	    end
-			if player:HasTrait("NimbleFingers2") then
+			if player:HasTrait("nimblefingers2") then
 	    	player:getInventory():AddItem("FMJ.BobbyPin");
 	    	player:getInventory():AddItem("FMJ.BobbyPin")
 			end
