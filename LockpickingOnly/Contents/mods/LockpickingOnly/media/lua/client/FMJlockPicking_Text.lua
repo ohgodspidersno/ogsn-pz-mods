dab14acf7cb9b9f44aa84aff96252f762ebc3144
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-10 23:06:47 -0400

    In LockpickingOnly, Fixes FMJ_Text to FMJlockPicking_Text that I changed when the mods were split apart

--- LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/FMJlockPicking_Text.lua
+++ LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/FMJlockPicking_Text.lua
@@ -1,8 +1,8 @@
 -- Contains all human readable text of the mod.
-FMJ_Text = {};
+FMJlockPicking_Text = {};
 
-FMJ_Text.lockLevels = { getText("UI_Text_locklevel_veryeasy"),getText("UI_Text_locklevel_easy"),getText("UI_Text_locklevel_medium"),getText("UI_Text_locklevel_hard"),getText("UI_Text_locklevel_veryhard"),getText("UI_Text_locklevel_willbreak") };
-FMJ_Text.brokenLockModal = getText("UI_Text_brokenLockModal");
-FMJ_Text.contextBreakDoorLock = getText("UI_Text_contextBreakDoorLock");
-FMJ_Text.contextPickDoorLock = getText("UI_Text_contextPickDoorLock");
-FMJ_Text.contextBreakWindowLock = getText("UI_Text_contextBreakWindowLock");
+FMJlockPicking_Text.lockLevels = { getText("UI_Text_locklevel_veryeasy"),getText("UI_Text_locklevel_easy"),getText("UI_Text_locklevel_medium"),getText("UI_Text_locklevel_hard"),getText("UI_Text_locklevel_veryhard"),getText("UI_Text_locklevel_willbreak") };
+FMJlockPicking_Text.brokenLockModal = getText("UI_Text_brokenLockModal");
+FMJlockPicking_Text.contextBreakDoorLock = getText("UI_Text_contextBreakDoorLock");
+FMJlockPicking_Text.contextPickDoorLock = getText("UI_Text_contextPickDoorLock");
+FMJlockPicking_Text.contextBreakWindowLock = getText("UI_Text_contextBreakWindowLock");
