937e57c126db7b51dd75973f6c45bc6a15974f7d
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-11 01:06:11 -0400

    In ForkMJ: Fixes misplaced end causing trait generation to fail, and also changes NimbleFingers to nimblefingers to match what it is earlier in the file.

--- ForkMJLockpicking/Contents/mods/FMJlockPicking/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
+++ ForkMJLockpicking/Contents/mods/FMJlockPicking/media/lua/client/Professions/FMJlockPicking_Traits_lockpick.lua
@@ -3,22 +3,23 @@ require('NPCs/MainCreationMethods');
 local function initTraits()
 
 	local nimblefingers = TraitFactory.addTrait("nimblefingers", getText("UI_trait_nimblefingers"), 0, getText("UI_trait_nimblefingersdesc"), true);
-	local nimblefingers = TraitFactory.addTrait("nimblefingers2", getText("UI_trait_nimblefingers"), 3, getText("UI_trait_nimblefingersdesc"), false, false);
-		TraitFactory.setMutualExclusive("nimblefingers", "nimblefingers2");
+	local nimblefingers2 = TraitFactory.addTrait("nimblefingers2", getText("UI_trait_nimblefingers"), 3, getText("UI_trait_nimblefingersdesc"), false, false);
+	TraitFactory.setMutualExclusive("nimblefingers", "nimblefingers2");
+end
 
 Events.OnGameBoot.Add(initTraits);
 
 if getActivatedMods():contains("oka_oiosb41") then
 	Events.OnNewGame.Add(
 		function(player, square)
-			if player:HasTrait("NimbleFingers") then
+			if player:HasTrait("nimblefingers") then
 	    	player:getInventory():AddItem("FMJ.BobbyPin");
 	    	player:getInventory():AddItem("FMJ.BobbyPin")
 	    end
-			if player:HasTrait("NimbleFingers2") then
+			if player:HasTrait("nimblefingers2") then
 	    	player:getInventory():AddItem("FMJ.BobbyPin");
 	    	player:getInventory():AddItem("FMJ.BobbyPin")
 			end
-		end)
-	end
+		end
+	)
 end
