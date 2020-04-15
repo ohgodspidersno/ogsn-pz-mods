e3367b7ca88f325794a27c5df4f567bdb7a79a6c
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-15 00:52:35 -0400

    Function to give existing burglars in old savegames the lockpicking mod recipes now works.

--- LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/FMJlockpicking_BackwardCompatibility.lua
+++ LockpickingOnly/Contents/mods/LockpickingOnly/media/lua/client/FMJlockpicking_BackwardCompatibility.lua
@@ -1,45 +1,21 @@
+require('luautils');
 -- This will give burglars in old saved games the recipes
-local function GiveTraitsToExistingBurglars(playerObj, playerSquare)
-    local learned_recipes = playerObj:getKnownRecipes();
-    if playerObj:getTraits():contains('Burglar') then
-        if not learned_recipes:contains("Lockpicking") then
-            learned_recipes:add("Lockpicking");
+Events.OnGameStart.Add(
+  function(player, square)
+    local playerObj = getSpecificPlayer(0)
+    if playerObj:HasTrait("Burglar") then
+        if not playerObj:getKnownRecipes():contains("Lockpicking") then
+            playerObj:getKnownRecipes():add("Lockpicking");
         end
-        if not learned_recipes:contains("Create Hairpin") then
-            learned_recipes:add("Create Hairpin");
+        if not playerObj:getKnownRecipes():contains("Create Hairpin") then
+            playerObj:getKnownRecipes():add("Create Hairpin");
         end
-        if not learned_recipes:contains("Break Door Locks") then
-            learned_recipes:add("Break Door Locks");
+        if not playerObj:getKnownRecipes():contains("Break Door Locks") then
+            playerObj:getKnownRecipes():add("Break Door Locks");
         end
-        if not learned_recipes:contains("Break Window Locks") then
-            learned_recipes:add("Break Window Locks");
+        if not playerObj:getKnownRecipes():contains("Break Window Locks") then
+            playerObj:getKnownRecipes():add("Break Window Locks");
         end
     end
-end
-Events.OnGameStart.Add( GiveTraitsToExistingBurglars );
-
--- REFERENCE SNIPPETS FROM ELSEWHERE
--- self.character:getKnownRecipes():add("Lockpicking");
--- self.character:getKnownRecipes():add("Create Hairpin");
--- self.character:getKnownRecipes():add("Break Door Locks");
--- self.character:getKnownRecipes():add("Break Window Locks");
--- playerObj:getInventory():AddItem(v);
--- :getTraits():contains(trait:getType()) then
-
--- ISSafetyUI.onNewGame = function(playerObj, playerSquare)
---     if not isClient() then return end
---     if getServerOptions():getOption("SpawnItems") and getServerOptions():getOption("SpawnItems")~= "" then
---         local items = luautils.split(getServerOptions():getOption("SpawnItems"), ",");
---         for i,v in pairs(items) do
---             playerObj:getInventory():AddItem(v);
---         end
---     end
--- end
-
-  -- playerObj:getInventory():AddItem(v);
-    -- if isServer() or isClient() then return end
-    -- local main = ErosionMain.getInstance()
-    -- if not main:getConfig():getDebug():getEnabled() then return end
-    -- EDebug.DemoTime(main):GameStart()
-
--- Events.OnNewGame.Add(ISSafetyUI.onNewGame);
+  end
+)
