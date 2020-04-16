e559af3e872d4a88b23567c60a1f9bb99fc20ade
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-15 23:34:00 -0400

    First pass of MakeCupHerbalTeaOGSN()

--- VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/server/recipecode_ogsn_herbs.lua
+++ VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/server/recipecode_ogsn_herbs.lua
@@ -1,3 +1,4 @@
+-- Note: ISCraftAction:perform() in the vanilla code is a good reference
 
 
 -- MakeCupHerbalTeaOGSN
@@ -6,6 +7,35 @@
 --
 --
 -- CookRawHerbOGSN
+function MakeCupHerbalTeaOGSN(items, result, player)
+-- pass on the burnt, rotten status of the herb/blend
+-- if it was burnt or rotten strip it of any positive effects
+  local rotten = false
+  local burnt = false
+  local oldest = 0
+  for i=0, items:size() -1 do
+    local ingredient = items:get(i)
+    if ingredient:isRotten() then
+      rotten = true
+    end
+    if ingredient:isBurnt() then
+      burnt = true
+    end
+    if ingredient:getAge() > oldest then
+      oldest = ingredient:getAge()
+    end
+    result:setRotten(rotten)
+    result:setBurnt(burnt)
+    result:setAge(oldest)
+    if rotten or burnt then
+      result:setFluReduction(0)
+      result:setReduceFoodSickness(0)
+      result:setPainReduction(0)
+      result:setEnduranceChange(0)
+      result:setFatigueChange(0)
+    end
+  end
+end
 -- if it was rotten, just let it keep cooking like a normal piece of rotten food
 -- if it was not rotten, replace it with the dried version
 --
