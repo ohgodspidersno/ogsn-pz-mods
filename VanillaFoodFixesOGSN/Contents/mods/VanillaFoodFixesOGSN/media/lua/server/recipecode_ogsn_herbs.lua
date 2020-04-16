3c101a9acccd2018a21b6c270925d3613f6a4346
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-15 21:37:27 -0400

    Adds new pseudocode comments to recipecode that I believe are what will be implemented

--- VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/server/recipecode_ogsn_herbs.lua
+++ VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/server/recipecode_ogsn_herbs.lua
@@ -1,12 +1,14 @@
--- If we go this route, do this:
--- Normal Herbs OnCook make dried versions that are already "cooked"
--- Normal Herbs OnRotten make rotten versions that are already "rotten"
--- Dried herbs OnBurn make burnt version that are already "burnt"
--- Remember to change translations so that (cooked) does not appear on dried herbs
 
 
-
--- Another Route (might not be feasible):
--- Scrap the rotten and burnt versions entirely
--- Normal herbs would have an OnCook lua that turns them into Dried versions, but only if they're NOT rotten.
--- Then there would be another lua that would make it so that brewing tea would yield a "Bad Herbal Tea" if the ingredient was rotten and/or burnt.
+-- MakeCupHerbalTeaOGSN
+-- pass on the burned, rotten status of the herb/blend
+-- if it was burned or rotten strip it of any positive effects
+--
+--
+-- CookRawHerbOGSN
+-- if it was rotten, just let it keep cooking like a normal piece of rotten food
+-- if it was not rotten, replace it with the dried version
+--
+-- MakeHerbalBlendOGSN
+-- if any of the ingredients were rotten and/or burned, make it rotten and/or burned
+-- if all of the ingredients were fresh, make it fresh
