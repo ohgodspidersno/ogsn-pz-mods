479c4144cdafdabe9c164858cfbb5e740ba66cea
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-16 00:58:43 -0400

    Fixes problem in tweaks where burn time was shorter than cook time, causing stuff to just burn halfway through cooking

--- VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_ogsn_herbs_Cooking.lua
+++ VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_ogsn_herbs_Cooking.lua
@@ -15,7 +15,7 @@ TweakItem("Base.CommonMallow","FoodType", "Herbal");
 TweakItem("Base.CommonMallow","EvolvedRecipe", "Soup:2;Stew:2;Pie:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2");
 TweakItem("Base.CommonMallow","IsCookable","True");
 TweakItem("Base.CommonMallow","MinutesToCook","120");
-TweakItem("Base.CommonMallow","MinutesToBurn","30");
+TweakItem("Base.CommonMallow","MinutesToBurn","150");
 TweakItem("Base.CommonMallow","OnCooked","CookRawHerbOGSN");
 
 TweakItem("Base.LemonGrass","Spice","True");
@@ -23,7 +23,7 @@ TweakItem("Base.LemonGrass","FoodType","Herbal");
 TweakItem("Base.LemonGrass","EvolvedRecipe","Soup:1;Stew:1;Pie:1;Cake:1;Stir fry:1;Stir fry Griddle Pan:1;Roasted Vegetables:1;Sandwich:1;Burger:1;Salad:1;FruitSalad:1;PastaPan:1;RicePan:1;PastaPot:1;RicePot:1");
 TweakItem("Base.LemonGrass","IsCookable","True");
 TweakItem("Base.LemonGrass","MinutesToCook","120");
-TweakItem("Base.LemonGrass","MinutesToBurn","30");
+TweakItem("Base.LemonGrass","MinutesToBurn","150");
 TweakItem("Base.LemonGrass","OnCooked","CookRawHerbOGSN");
 
 
@@ -32,7 +32,7 @@ TweakItem("Base.BlackSage","FoodType","Herbal");
 TweakItem("Base.BlackSage","EvolvedRecipe","Soup:1;Stew:1;Pie:1;Cake:1;Stir fry:1;Stir fry Griddle Pan:1;Roasted Vegetables:1;Burger:1;PastaPan:1;RicePan:1;PastaPot:1;RicePot:1");
 TweakItem("Base.BlackSage","IsCookable","True");
 TweakItem("Base.BlackSage","MinutesToCook","120");
-TweakItem("Base.BlackSage","MinutesToBurn","30");
+TweakItem("Base.BlackSage","MinutesToBurn","150");
 TweakItem("Base.BlackSage","OnCooked","CookRawHerbOGSN");
 
 
@@ -41,7 +41,7 @@ TweakItem("Base.Ginseng","FoodType","Herbal")
 TweakItem("Base.Ginseng","EvolvedRecipe","Soup:1;Stew:1;Pie:1;Cake:1;Stir fry:1;Stir fry Griddle Pan:1;Roasted Vegetables:1;Salad:1;PastaPan:1;RicePan:1;PastaPot:1;RicePot:1")
 TweakItem("Base.Ginseng","IsCookable","True");
 TweakItem("Base.Ginseng","MinutesToCook","120");
-TweakItem("Base.Ginseng","MinutesToBurn","30");
+TweakItem("Base.Ginseng","MinutesToBurn","150");
 TweakItem("Base.Ginseng","OnCooked","CookRawHerbOGSN");
 
 
@@ -50,7 +50,7 @@ TweakItem("Base.Rosehips","FoodType","Herbal");
 TweakItem("Base.Rosehips","EvolvedRecipe","Soup:3;Stew:3;Pie:3;Cake:3;Stir fry:3;Stir fry Griddle Pan:3;Roasted Vegetables:3;Burger:3;Salad:3;PastaPan:3;RicePan:3;PastaPot:3;RicePot:3;HotDrink:3;CommonMallowEvolved:3;LemonGrassEvolved:3;BlackSageEvolved:3;GinsengEvolved:3;EnergizingEvolved:3;MedicinalEvolved:3");
 TweakItem("Base.Rosehips","IsCookable","True");
 TweakItem("Base.Rosehips","MinutesToCook","120");
-TweakItem("Base.Rosehips","MinutesToBurn","30");
+TweakItem("Base.Rosehips","MinutesToBurn","150");
 TweakItem("Base.Rosehips","OnCooked","CookRawHerbOGSN");
 
 
@@ -58,7 +58,7 @@ TweakItem("Base.GrapeLeaves","FoodType","Herbal")
 TweakItem("Base.GrapeLeaves","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;CommonMallowEvolved:2;LemonGrassEvolved:2;BlackSageEvolved:2;GinsengEvolved:2;EnergizingEvolved:2;MedicinalEvolved:2")
 TweakItem("Base.GrapeLeaves","IsCookable","True");
 TweakItem("Base.GrapeLeaves","MinutesToCook","120");
-TweakItem("Base.GrapeLeaves","MinutesToBurn","30");
+TweakItem("Base.GrapeLeaves","MinutesToBurn","150");
 TweakItem("Base.GrapeLeaves","OnCooked","CookRawHerbOGSN");
 
 
@@ -66,7 +66,7 @@ TweakItem("Base.Violets","FoodType","Herbal")
 TweakItem("Base.Violets","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Cake:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Salad:2;FruitSalad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;CommonMallowEvolved:2;LemonGrassEvolved:2;BlackSageEvolved:2;GinsengEvolved:2;EnergizingEvolved:2;MedicinalEvolved:2")
 TweakItem("Base.Violets","IsCookable","True");
 TweakItem("Base.Violets","MinutesToCook","120");
-TweakItem("Base.Violets","MinutesToBurn","30");
+TweakItem("Base.Violets","MinutesToBurn","150");
 TweakItem("Base.Violets","OnCooked","CookRawHerbOGSN");
 
 -- COMPATIBILITY WITH OTHER FOOD MODS
