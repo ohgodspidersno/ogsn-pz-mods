b3e016747588008818067c31edf8fd8dc01cba47
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-16 20:25:54 -0400

    GIves burning, cooking, OnCooked attributes to wild garlic and plantains

--- VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_ogsn_herbs_Poultices.lua
+++ VanillaFoodFixesOGSN/Contents/mods/VanillaFoodFixesOGSN/media/lua/client/tweaks_ogsn_herbs_Poultices.lua
@@ -13,6 +13,9 @@ TweakItem("Base.Plantain","Calories","55");--(9+10+24)*1.28 (PZ's weird coeffici
 TweakItem("Base.Plantain","FoodType","Herbal");
 TweakItem("Base.Plantain","EvolvedRecipe","Soup:2;Stew:2;Pie:2;Cake:2;Stir fry:2;Stir fry Griddle Pan:2;Roasted Vegetables:2;Burger:2;Salad:2;PastaPan:2;RicePan:2;PastaPot:2;RicePot:2;HotDrink:2;CommonMallowEvolved:2;LemonGrassEvolved:2;BlackSageEvolved:2;GinsengEvolved:2");
 TweakItem("Base.Plantain","CantBeFrozen","False");
+TweakItem("Base.Plantain","MinutesToCook","120");
+TweakItem("Base.Plantain","MinutesToBurn","150");
+TweakItem("Base.Plantain","OnCooked","CookRawHerbOGSN");
 -- WildGarlic
 TweakItem("Base.WildGarlic","HungerChange","-1");
 TweakItem("Base.WildGarlic","Weight","0.1");
@@ -28,3 +31,6 @@ TweakItem("Base.WildGarlic","Spice","True");
 TweakItem("Base.WildGarlic","FoodType","Herbal");
 TweakItem("Base.WildGarlic","EvolvedRecipe","Soup:1;Stew:1;Pie:1;Stir fry:1;Stir fry Griddle Pan:1;Roasted Vegetables:1;Burger:1;Salad:1;PastaPan:1;RicePan:1;PastaPot:1;RicePot:1;HotDrink:1;CommonMallowEvolved:1;LemonGrassEvolved:1;BlackSageEvolved:1;GinsengEvolved:1");
 TweakItem("Base.WildGarlic","CantBeFrozen","False");
+TweakItem("Base.WildGarlic","MinutesToCook","120");
+TweakItem("Base.WildGarlic","MinutesToBurn","150");
+TweakItem("Base.WildGarlic","OnCooked","CookRawHerbOGSN");
