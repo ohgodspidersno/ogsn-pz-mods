7029ff8f50283b93e18be86d29907e57a68b2d19
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-18 03:40:09 -0400

    Adds an item tweaker to make OTR map appear if OTR mod is enabled. However I have not yet created the OTR version of the map yet.

---
+++ KnoxCountryMap_OGSN/Contents/mods/KnoxCountryMap_OGSN/media/lua/client/tweaks_ogsnKnoxMap.lua
@@ -0,0 +1,5 @@
+require("ItemTweaker_Copy_ogsn");
+if getActivatedMods():contains("CookWithHerbs") then
+    TweakItem("Base.KnoxCountryMap","Map","media/ui/LootableMaps/knox_country_map_OTR.png");
+    TweakItem("Base.KnoxCountryMapSecret","Map","media/ui/LootableMaps/knox_country_map_secret_OTR.png");
+else return end
