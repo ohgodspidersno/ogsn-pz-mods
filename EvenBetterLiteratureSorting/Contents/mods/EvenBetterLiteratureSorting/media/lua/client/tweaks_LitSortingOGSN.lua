3a314a0e2bdc8f7d97e7ec5d8cafb4d44f2c1109
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-18 21:54:25 -0400

    Comments out the redefinition of ripped pages until I've considered it more

--- EvenBetterLiteratureSorting/Contents/mods/EvenBetterLiteratureSorting/media/lua/client/tweaks_LitSortingOGSN.lua
+++ EvenBetterLiteratureSorting/Contents/mods/EvenBetterLiteratureSorting/media/lua/client/tweaks_LitSortingOGSN.lua
@@ -1,6 +1,6 @@
 -- if getActivatedMods():contains("ItemTweakerAPI") then
 require("ItemTweaker_Copy_ogsn");
 -- else return end
-if getActivatedMods():contains("RelaxingTime") then
-  TweakItem("filcher.RippedPages","Type","Normal")
-end
+-- if getActivatedMods():contains("RelaxingTime") then
+--   TweakItem("filcher.RippedPages","Type","Normal")
+-- end
