4dad34833e46e2e0fe0abe3c454f5482ccc3b412
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-19 05:48:27 -0400

    First page is 'marked' instantaneously, unless it is an appropriate level. Pre-marked books' first page will still be re-read when it comes time to read them for real.

--- LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/MakeOnePageReadable_copy_ogsn.lua
+++ LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/MakeOnePageReadable_copy_ogsn.lua
@@ -4,45 +4,24 @@
 local originalISReadABookUpdate = ISReadABook.update
 
 function ISReadABook:update(...)
-	if self.item:getNumberOfPages() > 0 and SkillBook[self.item:getSkillTrained()] then  -- it's a valid skillbook
-				if self.item:getAlreadyReadPages() == 0 then	-- if the player hasn't read it yet at all...
-						self.character:setAlreadyReadPages(self.item:getFullType(), 1)	-- ... they get the first page for free
-				end
-				if self.item:getLvlSkillTrained() > self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 or self.character:HasTrait("Illiterate") then
-				-- this if statement hits the requirements to burrow down into the 'i can't read this' block, so we can replace it
-				-- it's basically the same except I've removed the changes that already happened up to this point
-						self.pageTimer = 0;
-						if txtRandom == 0 then
-								self.character:Say(getText("IGUI_PlayerText_DontGet"));
-						elseif txtRandom == 1 then
-								self.character:Say(getText("IGUI_PlayerText_TooComplicated"));
-						else
-								self.character:Say(getText("IGUI_PlayerText_DontUnderstand"));
-						end
-						self:forceStop()
-				else
-					-- if they can read it, use the original function
-					return originalISReadABookUpdate(self, ...)
-				end
+	if self.item:getNumberOfPages() > 0 and
+	SkillBook[self.item:getSkillTrained()] and
+	self.item:getLvlSkillTrained() > self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1 or self.character:HasTrait("Illiterate") then
+	-- this hits the requirements to burrow down into the 'i can't read this' block
+			self.pageTimer = 0;
+			local txtRandom = ZombRand(3);
+			if txtRandom == 0 then
+					self.character:Say(getText("IGUI_PlayerText_DontGet"));
+			elseif txtRandom == 1 then
+					self.character:Say(getText("IGUI_PlayerText_TooComplicated"));
+			else
+					self.character:Say(getText("IGUI_PlayerText_DontUnderstand"));
+			end
+			if self.item:getNumberOfPages() > 0 then
+					self.character:setAlreadyReadPages(self.item:getFullType(), 1)
+					self:forceStop()
+			end
+	else
+		return originalISReadABookUpdate(self, ...)
 	end
-	return originalISReadABookUpdate(self, ...) -- it'll probably never get here but just in case, throw it back to the original
 end
-
---
---
--- function ISReadABook:update(...)
--- 			if SkillBook[self.item:getSkillTrained()] and self.item:getLvlSkillTrained() > (self.character:getPerkLevel(SkillBook[self.item:getSkillTrained()].perk) + 1) and self.item:getNumberOfPages() > 0 then
--- 						local characterMetaTable = getmetatable(self.character)
--- 						local originalSetAlreadyReadPages = characterMetaTable.__index.setAlreadyReadPages
--- 						characterMetaTable.__index.setAlreadyReadPages = function(character, fullType, value, ...)
--- 									if value == 0 then -- if self.pageTimer == 0 and value == 0 then
--- 												value = 1
--- 									end
--- 									return originalSetAlreadyReadPages(character, fullType, value, ...)
--- 						end
--- 						local result = originalISReadABookUpdate(self, ...)
--- 						characterMetaTable.__index.setAlreadyReadPages = originalSetAlreadyReadPages
--- 						return result
--- 			end
--- 			return originalISReadABookUpdate(self, ...)
--- end
