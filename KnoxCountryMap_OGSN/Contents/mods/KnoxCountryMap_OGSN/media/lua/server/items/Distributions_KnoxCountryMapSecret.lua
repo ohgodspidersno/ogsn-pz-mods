d92c30df13a67ad063bcf77abe85fc078ed29127
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-25 23:17:40 -0400

    Makes secret knox country map appear very rarely in very specific places

---
+++ KnoxCountryMap_OGSN/Contents/mods/KnoxCountryMap_OGSN/media/lua/server/items/Distributions_KnoxCountryMapSecret.lua
@@ -0,0 +1,29 @@
+require 'Items/SuburbsDistributions'
+require 'Vehicles/VehicleDistributions'
+
+-- SuburbsDistributions
+table.insert(SuburbsDistributions["all.desk"].items, "Base.KnoxCountryMapSecret");
+table.insert(SuburbsDistributions["all.desk"].items, 0.0005);
+
+table.insert(SuburbsDistributions["Bag_WeaponBag"].items, "Base.KnoxCountryMapSecret");
+table.insert(SuburbsDistributions["Bag_WeaponBag"].items, 1);
+
+-- VehicleDistributions
+table.insert(VehicleDistributions["Police.GloveBox"].items,"Base.KnoxCountryMapSecret");
+table.insert(VehicleDistributions["Police.GloveBox"].items, 0.1);
+
+table.insert(VehicleDistributions["Ranger.GloveBox"].items,"Base.KnoxCountryMapSecret");
+table.insert(VehicleDistributions["Ranger.GloveBox"].items, 0.2);
+
+table.insert(VehicleDistributions["Radio.GloveBox"].items,"Base.KnoxCountryMapSecret");
+table.insert(VehicleDistributions["Radio.GloveBox"].items, 100);
+
+if getActivatedMods():contains("MysteryMachineOGSN") then
+  table.insert(VehicleDistributions["MysteryMachine.Glovebox"].items,"Base.KnoxCountryMapSecret");
+  table.insert(VehicleDistributions["MysteryMachine.Glovebox"].items, 100);
+end
+-- table.insert(VehicleDistributions["GloveBox"].items, "filcher.PostcardLocal");
+-- table.insert(VehicleDistributions["GloveBox"].items, 1);
+
+-- policestorage = {
+--         locker ={
