52d2da46c0180a531408006e8c543817630746ef
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-25 23:49:13 -0400

    Temporarily sets Mystery Machine's vehicle zone chance ludicrously high for testing purposes

--- MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
+++ MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
@@ -6,17 +6,17 @@ if VehicleZoneDistribution then
   -- VehicleZoneDistribution.mysterymachine.specialCar = true;
 
   VehicleZoneDistribution.parkingstall.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
-  VehicleZoneDistribution.medium.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.good.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.trafficjamw.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
+  VehicleZoneDistribution.medium.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.good.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.trafficjamw.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
   VehicleZoneDistribution.police.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
   VehicleZoneDistribution.fire.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
   VehicleZoneDistribution.ranger.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
-  VehicleZoneDistribution.mccoy.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.postal.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.ambulance.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
-  VehicleZoneDistribution.fossoil.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = .001};
+  VehicleZoneDistribution.mccoy.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.postal.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.ambulance.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.fossoil.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
   VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
   VehicleZoneDistribution.radio.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
 end
