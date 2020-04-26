99e6918ace36e679c01323897294e3a7a14f02ab
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-25 23:10:28 -0400

    Removes conditional secret map distribution from MysteryMachine mod, going to put that in the Map mod instead

--- MysteryMachine/Contents/mods/MysteryMachine/media/lua/server/Vehicles/MysteryMachineOGSNVehicleDistributions.lua
+++ MysteryMachine/Contents/mods/MysteryMachine/media/lua/server/Vehicles/MysteryMachineOGSNVehicleDistributions.lua
@@ -3,113 +3,7 @@ require 'Vehicles/VehicleDistributions'
 
 local distributionTable = VehicleDistributions[1]
 
-if getActivatedMods():contains("KnoxCountryMap_OGSN") then
-  VehicleDistributions.MysteryMachine = {
-        GloveBox =
-        {
-                rolls = 1,
-                items = {
-                        "KnoxCountryMapSecret", 100,
-                }
-        },
-        TruckBed =
-        {
-               rolls = 3,
-               items = {
-                       "Bread", 7,
-                       "Steak", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Fries", 4,
-                       "Fries", 4,
-                       "Chicken", 3,
-                       "Ham", 3,
-                       "Cheese", 4,
-                       "Cheese", 4,
-                       "Pop", 4,
-                       "Pop2", 4,
-                       "Pop3", 4,
-                       "PopBottle", 3,
-                       "Lettuce", 3,
-                       "Mustard", 3,
-                       "Ketchup", 3,
-                       "Processedcheese", 3,
-                       "Processedcheese", 3,
-                       "farming.Cabbage", 4,
-                       "farming.Bacon", 4,
-                       "farming.Bacon", 4,
-               }
-        },
-        SeatRearLeft =
-        {
-               rolls = 3,
-               items = {
-                       "Bread", 7,
-                       "Steak", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Fries", 4,
-                       "Fries", 4,
-                       "Chicken", 3,
-                       "Ham", 3,
-                       "Cheese", 4,
-                       "Cheese", 4,
-                       "Pop", 4,
-                       "Pop2", 4,
-                       "Pop3", 4,
-                       "PopBottle", 3,
-                       "Lettuce", 3,
-                       "Mustard", 3,
-                       "Ketchup", 3,
-                       "Processedcheese", 3,
-                       "Processedcheese", 3,
-                       "farming.Cabbage", 4,
-                       "farming.Bacon", 4,
-                       "farming.Bacon", 4,
-               }
-        },
-        SeatRearRight =
-        {
-               rolls = 3,
-               items = {
-                       "Bread", 7,
-                       "Steak", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Burger", 4,
-                       "Fries", 4,
-                       "Fries", 4,
-                       "Chicken", 3,
-                       "Ham", 3,
-                       "Cheese", 4,
-                       "Cheese", 4,
-                       "Pop", 4,
-                       "Pop2", 4,
-                       "Pop3", 4,
-                       "PopBottle", 3,
-                       "Lettuce", 3,
-                       "Mustard", 3,
-                       "Ketchup", 3,
-                       "Processedcheese", 3,
-                       "Processedcheese", 3,
-                       "farming.Cabbage", 4,
-                       "farming.Bacon", 4,
-                       "farming.Bacon", 4,
-               }
-  },
-else
-  VehicleDistributions.MysteryMachine = {
+VehicleDistributions.MysteryMachine = {
        TruckBed =
        {
                rolls = 3,
@@ -206,6 +100,5 @@ else
                        "farming.Bacon", 4,
                }
        },
-end
 
 distributionTable["mysterymachine"] = { Normal = VehicleDistributions.mysterymachine; }
