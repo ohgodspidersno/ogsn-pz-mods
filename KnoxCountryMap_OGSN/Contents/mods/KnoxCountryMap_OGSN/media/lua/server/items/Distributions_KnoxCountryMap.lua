a8ad08c68a881b5cacf3b0f2b6bb7f35829c4290
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-25 22:59:13 -0400

    Knox country map to vehicle distribution table

--- KnoxCountryMap_OGSN/Contents/mods/KnoxCountryMap_OGSN/media/lua/server/items/Distributions_KnoxCountryMap.lua
+++ KnoxCountryMap_OGSN/Contents/mods/KnoxCountryMap_OGSN/media/lua/server/items/Distributions_KnoxCountryMap.lua
@@ -30,6 +30,9 @@ table.insert(SuburbsDistributions["all"]["sidetable"].items, 0.01);
 table.insert(SuburbsDistributions["Bag_SurvivorBag"].items, "Base.KnoxCountryMap");
 table.insert(SuburbsDistributions["Bag_SurvivorBag"].items, 30);
 
+table.insert(VehicleDistributions["GloveBox"].items, "Base.KnoxCountryMap");
+table.insert(VehicleDistributions["GloveBox"].items, 5);
+
 -- table.insert(SuburbsDistributions["all"]["inventorymale"].items, "Base.KnoxCountryMap");
 -- table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.1);
 -- table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "Base.KnoxCountryMap");
