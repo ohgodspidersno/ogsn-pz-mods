6203f339361ac0b7f0d27c108b8a290f319c4b42
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-25 21:18:50 -0400

    Cleans up the vehicle zone definitions lua

--- MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/VehicleZoneDefinition.lua
+++ MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/VehicleZoneDefinition.lua
@@ -1,10 +1,11 @@
-VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 20};
+if VehicleZoneDistribution then
+  -- mysterymachine zone
+  VehicleZoneDistribution.mysterymachine = {};
+  VehicleZoneDistribution.mysterymachine.vehicles = {};
+  VehicleZoneDistribution.mysterymachine.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
+  VehicleZoneDistribution.mysterymachine.specialCar = true;
 
--- mysterymachine
-VehicleZoneDistribution.mysterymachine = {};
-VehicleZoneDistribution.mysterymachine.vehicles = {};
-VehicleZoneDistribution.mysterymachine.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 100};
-VehicleZoneDistribution.mysterymachine.specialCar = true;
-
-
-VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 10};
+  -- also may rarely appear in other zones
+  VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+  VehicleZoneDistribution.radio.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+end
