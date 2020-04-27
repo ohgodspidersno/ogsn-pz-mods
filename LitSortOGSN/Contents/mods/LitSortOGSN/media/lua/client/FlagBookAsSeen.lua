3d9cb63e64612cd15a414ae929dc8662be9f86b4
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-27 00:00:57 -0400

    Creates stop()

--- LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/FlagBookAsSeen.lua
+++ LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/FlagBookAsSeen.lua
@@ -12,5 +12,19 @@ end
 
 function ISReadABook:start()
 end
-function ISReadABook:stop()
+function ISFlagABookAsSeen:stop()
+  if self.item:getNumberOfPages() > 0 then -- check if valid
+
+    if self.item:getAlreadyReadPages() > 1 then
+      -- if they've somehow read more than 1 page, don't do anything
+      print('player had already read some of it. how did they get here?')
+    else
+      -- otherwise, immediately set the number of read pages to 1
+      self.item:setAlreadyReadPages(1)
+    end
+    -- then stop the action
+    self.item:setJobDelta(0.0);
+    ISBaseTimedAction.stop(self);
+  end
+end
 end
