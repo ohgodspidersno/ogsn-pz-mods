6f34e3843d2356119ce14db9e5b09edd808658fa
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-26 23:58:45 -0400

    Fixes isValid()

--- LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/FlagBookAsSeen.lua
+++ LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/FlagBookAsSeen.lua
@@ -4,7 +4,10 @@ ISFlagABookAsSeen = ISBaseTimedAction:derive("ISFlagABookAsSeen");
 
 function ISFlagABookAsSeen:isValid()
   -- player must have it, it must be a real valid book, and it must be totally unread
-  return self.character:getInventory():contains(self.item) and self.item:getNumberOfPages() > 0 and self.item:getAlreadyReadPages() < 0
+  return self.character:getInventory():contains(self.item)
+      and self.item:getNumberOfPages() > 0
+      and self.item:getAlreadyReadPages() < 1
+      and not self.character:HasTrait("Illiterate")
 end
 
 function ISReadABook:start()
