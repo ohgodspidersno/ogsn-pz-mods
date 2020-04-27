e42598be4559845104b9712a81bbdac072b05468
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-26 23:15:40 -0400

    Beginning work to rewrite book flagging mechanism from the ground up

---
+++ LitSortOGSN/Contents/mods/LitSortOGSN/media/lua/client/FlagBookAsSeen.lua
@@ -0,0 +1,13 @@
+require "TimedActions/ISBaseTimedAction"
+
+ISFlagABookAsSeen = ISBaseTimedAction:derive("ISFlagABookAsSeen");
+
+function ISFlagABookAsSeen:isValid()
+  -- player must have it, it must be a real valid book, and it must be totally unread
+  return self.character:getInventory():contains(self.item) and self.item:getNumberOfPages() > 0 and self.item:getAlreadyReadPages() < 0
+end
+
+function ISReadABook:start()
+end
+function ISReadABook:stop()
+end
