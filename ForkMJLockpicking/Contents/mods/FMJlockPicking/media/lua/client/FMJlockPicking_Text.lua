a7d43e90ce7578f731ef74392701d4049b8faa7c
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-04-29 03:15:03 -0400

    Removes dairy food and lockpicking submods from ForkMJ, since those have been solidly replaced by other mods on the workshop and are just wasting space on my list now.

--- ForkMJLockpicking/Contents/mods/FMJlockPicking/media/lua/client/FMJlockPicking_Text.lua
+++
@@ -1,8 +0,0 @@
--- Contains all human readable text of the mod.
-FMJlockPicking_Text = {};
-
-FMJlockPicking_Text.lockLevels = { getText("UI_Text_locklevel_veryeasy"),getText("UI_Text_locklevel_easy"),getText("UI_Text_locklevel_medium"),getText("UI_Text_locklevel_hard"),getText("UI_Text_locklevel_veryhard"),getText("UI_Text_locklevel_willbreak") };
-FMJlockPicking_Text.brokenLockModal = getText("UI_Text_brokenLockModal");
-FMJlockPicking_Text.contextBreakDoorLock = getText("UI_Text_contextBreakDoorLock");
-FMJlockPicking_Text.contextPickDoorLock = getText("UI_Text_contextPickDoorLock");
-FMJlockPicking_Text.contextBreakWindowLock = getText("UI_Text_contextBreakWindowLock");
