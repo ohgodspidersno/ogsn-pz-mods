5bf973f923e1853ec2be88da8c51d7c9826c516f
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-05 02:45:33 -0400

    Copies the base mod lua more closely

--- SpecialModeWeather_OGSN/Contents/mods/StormsAreComing_OGSN/lua/client/StormsAreComing.lua
+++ SpecialModeWeather_OGSN/Contents/mods/StormsAreComing_OGSN/lua/client/StormsAreComing.lua
@@ -1,20 +1,62 @@
-local function SAC_minutes()
-  getClimateManager():triggerCustomWeather(0.95, true);
+--***********************************************************
+--**                    THE INDIE STONE                    **
+--**				  Author: turbotutone				   **
+--***********************************************************
+
+AStormIsComing = {}
+
+
+AStormIsComing.OnGameStart = function()
+    --[[
+    local modal = ISModalRichText:new(getCore():getScreenWidth()/2 - 100, getCore():getScreenHeight()/2 - 50, 200, 100, getText("Challenge_AStormIsComingInfoBox"), false, nil, nil, 0);
+    modal:initialise();
+    modal:addToUIManager();
+    if JoypadState.players[1] then
+        JoypadState.players[1].focus = modal
+    end
+    --]]
 end
 
-local function SAC_seasons(_season)
-  _season:init(
-      25, --aprox miami florida
-      16, --min
-      30, --max
-      4, --amount of degrees temp can go lower or higher then mean
-      _season:getSeasonLag(),
-      _season:getHighNoon(),
-      _season:getSeedA(),
-      _season:getSeedB(),
-      _season:getSeedC()
-  );
+AStormIsComing.OnInitSeasons = function(_season)
+    _season:init(
+        25, --aprox miami florida
+        16, --min
+        30, --max
+        4, --amount of degrees temp can go lower or higher then mean
+        _season:getSeasonLag(),
+        _season:getHighNoon(),
+        _season:getSeedA(),
+        _season:getSeedB(),
+        _season:getSeedC()
+    );
 end
 
-Events.EveryTenMinutes.Add(SAC_minutes);
-Events.OnInitSeasons.Add(SAC_seasons);
+AStormIsComing.OnInitWorld = function()
+    SandboxVars.DayLength = 3;
+    SandboxVars.StartMonth = 7;
+    SandboxVars.StartTime = 2;
+    SandboxVars.Temperature = 3;
+    SandboxVars.Rain = 3;
+    -- SandboxVars.TimeSinceApo = 1;
+    Events.OnGameStart.Add(AStormIsComing.OnGameStart);
+    --Events.EveryDays.Add(AStormIsComing.EveryDays);
+    Events.EveryTenMinutes.Add(AStormIsComing.EveryTenMinutes);
+    Events.OnInitSeasons.Add(AStormIsComing.OnInitSeasons);
+end
+
+AStormIsComing.EveryTenMinutes = function()
+    getClimateManager():triggerCustomWeather(0.95, true);
+end
+
+
+AStormIsComing.RemovePlayer = function(p)
+end
+
+AStormIsComing.AddPlayer = function(p)
+
+end
+
+AStormIsComing.Render = function()
+end
+
+AStormIsComing.hourOfDay = 7;
--- SpecialModeWeather_OGSN/Contents/mods/ThePlummetingFog_OGSN/lua/client/ThePlummetingFog.lua
+++ SpecialModeWeather_OGSN/Contents/mods/ThePlummetingFog_OGSN/lua/client/ThePlummetingFog.lua
@@ -1,20 +1,61 @@
-local function TPF_minutes()
-  -- getClimateManager():triggerCustomWeather(0.95, true);
+--***********************************************************
+--**                    THE INDIE STONE                    **
+--**				  Author: turbotutone				   **
+--***********************************************************
+
+TheDescendingFog = {}
+
+TheDescendingFog.OnGameStart = function()
+    --[[
+    local modal = ISModalRichText:new(getCore():getScreenWidth()/2 - 100, getCore():getScreenHeight()/2 - 50, 200, 100, getText("Challenge_TheDescendingFogInfoBox"), false, nil, nil, 0);
+    modal:initialise();
+    modal:addToUIManager();
+    if JoypadState.players[1] then
+        JoypadState.players[1].focus = modal
+    end
+    --]]
 end
 
-local function TPF_seasons(_season)
-  _season:init(
-      _season:getLat(), --aprox miami florida
-      _season:getTempMin(), --min
-      _season:getTempMax(), --max
-      _season:getTempDiff(), --amount of degrees temp can go lower or higher then mean
-      _season:getSeasonLag(),
-      _season:getHighNoon(),
-      _season:getSeedA(),
-      _season:getSeedB(),
-      _season:getSeedC()
-  );
+TheDescendingFog.OnInitSeasons = function(_season)
+    _season:init(
+        _season:getLat(), --aprox miami florida
+        _season:getTempMin(), --min
+        _season:getTempMax(), --max
+        _season:getTempDiff(), --amount of degrees temp can go lower or higher then mean
+        _season:getSeasonLag(),
+        _season:getHighNoon(),
+        _season:getSeedA(),
+        _season:getSeedB(),
+        _season:getSeedC()
+    );
 end
 
-Events.EveryTenMinutes.Add(TPF_minutes);
-Events.OnInitSeasons.Add(TPF_seasons);
+TheDescendingFog.OnInitWorld = function()
+    SandboxVars.DayLength = 3;
+    SandboxVars.StartMonth = 7;
+    SandboxVars.StartTime = 2;
+    SandboxVars.Temperature = 3;
+    SandboxVars.Rain = 3;
+    SandboxVars.TimeSinceApo = 1;
+    Events.OnGameStart.Add(TheDescendingFog.OnGameStart);
+    --Events.EveryDays.Add(TheDescendingFog.EveryDays);
+    Events.EveryTenMinutes.Add(TheDescendingFog.EveryTenMinutes);
+    Events.OnInitSeasons.Add(TheDescendingFog.OnInitSeasons);
+end
+
+TheDescendingFog.EveryTenMinutes = function()
+    --getClimateManager():triggerCustomWeather(0.95, true);
+end
+
+
+TheDescendingFog.RemovePlayer = function(p)
+end
+
+TheDescendingFog.AddPlayer = function(p)
+
+end
+
+TheDescendingFog.Render = function()
+end
+
+TheDescendingFog.hourOfDay = 7;
