d7f167abc86f0b18c0f630bdce852ae44e292f06
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-06 02:01:01 -0400

    Changes fog code. It now finds a new goal fog intensity a the start of every day, and it will slowly creep toward that goal every ten minutes for the next 24 hours.

--- SpecialModeWeather_OGSN/Contents/mods/TheDescendingFogs/media/client/lua/TheDescendingFogs.lua
+++ SpecialModeWeather_OGSN/Contents/mods/TheDescendingFogs/media/client/lua/TheDescendingFogs.lua
@@ -4,49 +4,43 @@ local fog = cliim:getClimateFloat(FOG_ID);
 local starting_strength = 0.3;
 local max_fog = 1;
 local min_fog = 0.3;
+local df = 0.01; -- the step size for fog intensity's change every ten minutes
 
-local fog_strength = starting_strength
+local fog_strength = starting_strength  -- fog_strength is the current intensity
+local fog_trend = max_fog -- this will update daily and will determine where the intensity that fog will gradually creep toward over the course of the day
 
-local function getNextFogStrength(old_strength)
-    local max = max_fog
-    local min = min_fog
-    local new_strength = (ZombRand(min*10,max*10)+ZombRand(min*10,max*10))/20
-    print('new_strength',new_strength)
+local function setFogTrend()
+  fog_trend = (ZombRand(min*10,max*10)+ZombRand((min+1)*10,max*10))/20 -- makes two rolls of the dice, designed to skew slightly higher than the median
 
-    -- if new_strength == max or new_strength == min then
-      -- fog_strength = new_strength -- on rare occasion it will get suddenly really clear or totally fogg
-    -- else
-    local df = new_strength - old_strength
-    print('df:',df)
-    if df > 0 then
-      fog_strength = math.min(old_strength+0.001,max)  -- trends up, unless it's already at full power
-    else
-      fog_strength = math.max(old_strength-0.001,min)  -- trends down, unless it's already at min power
+local function setNextFogStrength()
+    local delta = fog_trend - fog_strength
+    if delta > 0 then fog_strength = math.min(fog_strength + df, max)  -- trends up, unless it's already at full power
+    elseif delta < 0 then fog_strength = math.max(fog_strength - df, min)  -- trends down, unless it's already at min power
     end
-    -- end
-    print('just finished getNextFogStrength:',fog_strength)
-    return fog_strength
+    print('just finished setNextFogStrength:',fog_strength)
 end
 
 local function updateFog()
-  -- self.character:Say(tostring(fog_strength))
   print('fog was:',fog_strength)
   fog:setEnableAdmin(true);
   fog:setAdminValue(fog_strength);
-  getNextFogStrength(fog_strength)
-  -- self.character:Say(tostring(fog_strength))
+  setNextFogStrength(fog_strength)
 end
 
 Events.OnGameStart.Add(
   function()
       fog:setEnableAdmin(true);
-      fog_strength = fog:getAdminValue()
-      if fog_strength < min_fog or fog_strength > max_fog then fog_strength = starting_strength end -- in case something weird happens it will reset
+      if pcall(fog_strength = fog:getAdminValue()) -- protected call, if no error then it returns true
+      then -- do nothing special if nothing went wrong
+      else fog_strength = starting_strength end -- if there was an error set current strength to the default starting value
+      -- Now check for other weirdnesses, like if it was too big/small or if the min/max changed since last time the game was opened
+      if fog_strength < min_fog then fog_strength = min_fog
+      elseif fog_strength > max_fog then fog_strength = max_fog end -- in case something weird happens it will reset
   end
 );
 
-Events.EveryHours.Add(
-  -- updateFog
+Events.EveryDays.Add(
+  setNextFogStrength
 );
 
 Events.EveryTenMinutes.Add(
