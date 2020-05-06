8581c567f36e5d60071ea296b9395ddc558d3e98
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-06 01:31:14 -0400

    adding in a few lines

--- SpecialModeWeather_OGSN/Contents/mods/TheDescendingFogs/media/client/lua/TheDescendingFogs.lua
+++ SpecialModeWeather_OGSN/Contents/mods/TheDescendingFogs/media/client/lua/TheDescendingFogs.lua
@@ -1,1 +1,12 @@
-hi
+local cliim = getClimateManager();
+local FOG_ID = 5;
+local fog = cliim:getClimateFloat(FOG_ID);
+local starting_strength = 0.3;
+local max_fog = 1;
+local min_fog = 0.3;
+
+local fog_strength = starting_strength
+
+local function getNextFogStrength()
+    local max = max_fog
+    local min = min_fog
