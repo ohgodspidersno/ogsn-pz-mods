d27e6c531ed8be647f9ec1e6a6f0f7f612fb4531
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-08 23:32:47 -0400

    backs up FM2

---
+++ FashionMontage2beta/Contents/mods/FashionMontage2_Colors/media/lua/client/Definitions/OGSNfashionMontage.lua
@@ -0,0 +1,415 @@
+local listHat = {
+  "DistinctNames2.Hat_Bandana",
+  "DistinctNames2.Hat_BandanaTied",
+  "DistinctNames2.Hat_BandanaTINT",
+  "DistinctNames2.Hat_BandanaTiedTINT",
+  "DistinctNames2.Hat_Beany",
+  "DistinctNames2.Hat_Beret",
+  "DistinctNames2.Hat_BonnieHat",
+  "DistinctNames2.Hat_BonnieHat_CamoGreen",
+  "DistinctNames2.Hat_ChefHat",
+  "DistinctNames2.Hat_EarMuffs",
+  "DistinctNames2.Hat_EarMuff_Protectors",
+  "DistinctNames2.Hat_Fedora",
+  "DistinctNames2.Hat_Fedora_Delmonte",
+  "DistinctNames2.Hat_Police_Grey",
+  "DistinctNames2.Hat_Police",
+  "DistinctNames2.Hat_Ranger",
+  "DistinctNames2.Hat_HardHat",
+  "DistinctNames2.Hat_Army",
+  "DistinctNames2.Hat_BaseballHelmet_KY",
+  "DistinctNames2.Hat_BaseballHelmet_Rangers",
+  "DistinctNames2.Hat_BaseballHelmet_Z",
+  "DistinctNames2.Hat_BicycleHelmet",
+  "DistinctNames2.Hat_CrashHelmet",
+  "DistinctNames2.Hat_CrashHelmet_Police",
+  "DistinctNames2.Hat_CrashHelmet_Stars",
+  "DistinctNames2.Hat_Fireman",
+  "DistinctNames2.Hat_HockeyHelmet",
+  "DistinctNames2.Hat_JockeyHelmet01",
+  "DistinctNames2.Hat_JockeyHelmet02",
+  "DistinctNames2.Hat_JockeyHelmet03",
+  "DistinctNames2.Hat_JockeyHelmet04",
+  "DistinctNames2.Hat_JockeyHelmet05",
+  "DistinctNames2.Hat_JockeyHelmet06",
+  "DistinctNames2.Hat_RidingHelmet",
+  "DistinctNames2.Hat_GolfHatTINT",
+  "DistinctNames2.Hat_GolfHat",
+  "DistinctNames2.Hat_ShowerCap",
+  "DistinctNames2.Hat_PeakedCapArmy",
+  "DistinctNames2.Hat_WeddingVeil",
+  "DistinctNames2.Hat_WinterHat",
+  "DistinctNames2.Hat_BaseballCap",
+  "DistinctNames2.Hat_BaseballCap_Reverse",
+  "DistinctNames2.Hat_BaseballCapSolid",
+  "DistinctNames2.Hat_BaseballCapSolid_Reverse",
+  "DistinctNames2.Hat_BaseballCapKY",
+  "DistinctNames2.Hat_BaseballCapKY_Reverse",
+  "DistinctNames2.Hat_BeretArmy",
+  "DistinctNames2.Hat_Cowboy",
+  "DistinctNames2.Hat_FastFood",
+  "DistinctNames2.Hat_FastFood_IceCream",
+  "DistinctNames2.Hat_FastFood_Spiffo",
+  "DistinctNames2.Hat_Raccoon",
+  "DistinctNames2.Hat_SantaHat",
+  "DistinctNames2.Hat_SantaHatGreen",
+  "DistinctNames2.Hat_Sweatband",
+  "DistinctNames2.Hat_SummerHat",
+  "DistinctNames2.Hat_SurgicalCap_Green",
+  "DistinctNames2.Hat_WoolyHat",
+  "DistinctNames2.Hat_Visor_WhiteTINT",
+}
+local listTankTop = {
+  "DistinctNames2.Vest_DefaultTEXTURE_TINT",
+}
+local listTshirt = {
+  "DistinctNames2.Tshirt_WhiteTINT",
+  "DistinctNames2.Tshirt_WhiteLongSleeveTINT",
+  "DistinctNames2.Tshirt_PoloStripedTINT",
+  "DistinctNames2.Tshirt_PoloTINT",
+  "DistinctNames2.Tshirt_DefaultDECAL_TINT",
+  "DistinctNames2.Tshirt_Sport",
+  "DistinctNames2.Tshirt_ArmyGreen",
+  "DistinctNames2.Tshirt_CamoGreen",
+  "DistinctNames2.Tshirt_CamoUrban",
+  "DistinctNames2.Tshirt_Fossoil",
+  "DistinctNames2.Tshirt_Gas2Go",
+  "DistinctNames2.Tshirt_McCoys",
+  "DistinctNames2.Tshirt_PileOCrepe",
+  "DistinctNames2.Tshirt_PizzaWhirled",
+  "DistinctNames2.Tshirt_BusinessSpiffo",
+  "DistinctNames2.Tshirt_SpiffoDECAL",
+  "DistinctNames2.Tshirt_ThunderGas",
+  "DistinctNames2.Tshirt_ValleyStation",
+  "DistinctNames2.Tshirt_IndieStoneDECAL",
+  "DistinctNames2.Tshirt_Profession_FiremanBlue",
+  "DistinctNames2.Tshirt_Profession_FiremanRed02",
+  "DistinctNames2.Tshirt_Profession_FiremanRed",
+  "DistinctNames2.Tshirt_Profession_FiremanWhite",
+  "DistinctNames2.Tshirt_Scrubs",
+  "DistinctNames2.Tshirt_Ranger",
+  "DistinctNames2.Tshirt_Profession_RangerBrown",
+  "DistinctNames2.Tshirt_Profession_RangerGreen",
+  "DistinctNames2.Tshirt_Profession_PoliceBlue",
+  "DistinctNames2.Tshirt_Profession_PoliceWhite",
+  "DistinctNames2.Tshirt_PoliceGrey",
+  "DistinctNames2.Tshirt_PoliceBlue",
+  "DistinctNames2.Tshirt_Profession_VeterenGreen",
+  "DistinctNames2.Tshirt_Profession_VeterenRed",
+}
+local listShirt = {
+  "DistinctNames2.Shirt_FormalWhite",
+  "DistinctNames2.Shirt_Denim",
+  "DistinctNames2.Shirt_FormalWhite_ShortSleeve",
+  "DistinctNames2.Shirt_CamoGreen",
+  "DistinctNames2.Shirt_CamoUrban",
+  "DistinctNames2.Shirt_Baseball_KY",
+  "DistinctNames2.Shirt_Baseball_Rangers",
+  "DistinctNames2.Shirt_Baseball_Z",
+  "DistinctNames2.Shirt_HawaiianTINT",
+  "DistinctNames2.Shirt_HawaiianRed",
+  "DistinctNames2.Shirt_Jockey01",
+  "DistinctNames2.Shirt_Jockey02",
+  "DistinctNames2.Shirt_Jockey03",
+  "DistinctNames2.Shirt_Jockey04",
+  "DistinctNames2.Shirt_Jockey05",
+  "DistinctNames2.Shirt_Jockey06",
+  "DistinctNames2.Shirt_Lumberjack",
+  "DistinctNames2.Shirt_Scrubs",
+  "DistinctNames2.Shirt_Ranger",
+  -- "DistinctNames2.Shirt_OfficerWhite",
+  "DistinctNames2.Shirt_PoliceBlue",
+  "DistinctNames2.Shirt_PoliceGrey",
+  "DistinctNames2.Shirt_PrisonGuard",
+  "DistinctNames2.Shirt_Priest",
+  -- "DistinctNames2.Shirt_Workman",
+}
+local listSocks = {
+  "DistinctNames2.Socks_Ankle",
+  "DistinctNames2.Socks_Long",
+}
+local listPants = {
+  "DistinctNames2.Shorts_ShortFormal",
+  "DistinctNames2.Trousers_WhiteTINT",
+  "DistinctNames2.Trousers_Denim",
+  "DistinctNames2.Trousers_JeanBaggy",
+  "DistinctNames2.TrousersMesh_DenimLight",
+  -- "DistinctNames2.Trousers_ArmyService",
+  "DistinctNames2.Trousers_CamoGreen",
+  "DistinctNames2.Trousers_CamoUrban",
+  -- "DistinctNames2.Trousers_NavyBlue",
+  "DistinctNames2.Dungarees",
+  "DistinctNames2.Trousers_Chef",
+  "DistinctNames2.Trousers_Fireman",
+  -- "DistinctNames2.Trousers_Ranger",
+  -- "DistinctNames2.Trousers_PoliceGrey",
+  -- "DistinctNames2.Trousers_Police",
+  -- "DistinctNames2.Trousers_PrisonGuard",
+  "DistinctNames2.Trousers_Santa",
+  "DistinctNames2.Trousers_SantaGReen",
+  "DistinctNames2.Trousers_Scrubs",
+  "DistinctNames2.Trousers_SuitWhite",
+  "DistinctNames2.Trousers_Padded",
+  "DistinctNames2.Shorts_CamoGreenLong",
+  "DistinctNames2.Shorts_CamoColorLong", -- this is color camo long shorts
+  "DistinctNames2.Shorts_ShortDenim",
+  "DistinctNames2.Shorts_LongDenim",
+  "DistinctNames2.Shorts_LongDenimColors", --"DistinctNames2.Shorts_LongSport_Red",
+  "DistinctNames2.Shorts_ShortSport",
+  "DistinctNames2.Shorts_LongSport",
+  -- "DistinctNames2.Trousers_Black",
+}
+local listSkirt = {
+  "DistinctNames2.Skirt_Knees",
+  "DistinctNames2.Skirt_Long",
+  "DistinctNames2.Skirt_Normal",
+  "DistinctNames2.Skirt_Short",
+}
+local listDress = {
+  "DistinctNames2.Dress_Long",
+  "DistinctNames2.Dress_Normal",
+  "DistinctNames2.Dress_Knees",
+  "DistinctNames2.Dress_Short",
+  "DistinctNames2.HospitalGown",
+}
+local listShoes = {
+  "DistinctNames2.Shoes_ArmyBoots",
+  "DistinctNames2.Shoes_ArmyBootsDesert",
+  -- "DistinctNames2.Shoes_Black",
+  "DistinctNames2.Shoes_Brown",
+  "DistinctNames2.Shoes_Slippers",
+  "DistinctNames2.Shoes_TrainerTINT",
+  "DistinctNames2.Shoes_RidingBoots",
+  "DistinctNames2.Shoes_BlackBoots",
+  "DistinctNames2.Shoes_Wellies",
+}
+local listEyes = {
+  "DistinctNames2.Glasses_Normal",
+  "DistinctNames2.Glasses_Reading",
+  "DistinctNames2.Glasses_Sun",
+  "DistinctNames2.Glasses_Aviators",
+  "DistinctNames2.Glasses_SafetyGoggles",
+  "DistinctNames2.Glasses_Shooting",
+  "DistinctNames2.Glasses",
+  "DistinctNames2.Glasses_SkiGoggles",
+  "DistinctNames2.Glasses_SwimmingGoggles",
+}
+local listBeltExtra = {
+  "DistinctNames2.HolsterSimple",
+  "DistinctNames2.HolsterDouble",
+}
+local listMask = {
+  "DistinctNames2.Hat_BalaclavaFull",
+  "DistinctNames2.Hat_BalaclavaFace",
+  "DistinctNames2.Hat_BandanaMask",
+  "DistinctNames2.Hat_BandanaMaskTINT",
+  "DistinctNames2.Hat_DustMask",
+  "DistinctNames2.Hat_SurgicalMask_Green",
+}
+local listMaskEyes = {
+  "DistinctNames2.Hat_GasMask",
+  "DistinctNames2.WeldingMask",
+}
+local listUnderwear = {
+  "DistinctNames2.Bikini_TINT",
+  "DistinctNames2.Bikini_Pattern01",
+  "DistinctNames2.Swimsuit_TINT",
+  "DistinctNames2.SwimTrunks_Blue",
+}
+local listFullHat = {
+  "DistinctNames2.Hat_FootballHelmet",
+  "DistinctNames2.Hat_CrashHelmetFULL",
+  "DistinctNames2.Hat_RiotHelmet",
+  "DistinctNames2.Hat_SPHhelmet",
+  "DistinctNames2.Hat_NBCmask",
+  "DistinctNames2.Hat_Spiffo",
+}
+local listTorso1Legs1 = {
+  "DistinctNames2.LongJohns",
+}
+local listNeck = {
+  "DistinctNames2.Tie_BowTieFull",
+  -- "DistinctNames2.Tie_BowTieWorn",
+  "DistinctNames2.Tie_Full",
+  -- "DistinctNames2.Tie_Worn",
+  "DistinctNames2.Tie_Full_Spiffo",
+  -- "DistinctNames2.Tie_Worn_Spiffo",
+}
+local listHands = {
+  "DistinctNames2.Gloves_FingerlessGloves",
+  "DistinctNames2.Gloves_WhiteTINT",
+  "DistinctNames2.Gloves_LongWomenGloves",
+  "DistinctNames2.Gloves_LeatherGlovesBlack",
+  "DistinctNames2.Gloves_LeatherGlovesBlack",
+  "DistinctNames2.Gloves_LeatherGloves",
+}
+local listLegs1 = {
+  "DistinctNames2.LongJohns_Bottoms",
+}
+local listSweater = {
+  "DistinctNames2.Jumper_DiamondPatternTINT",
+  "DistinctNames2.Jumper_TankTopDiamondTINT",
+  "DistinctNames2.HoodieDOWN_WhiteTINT",
+  "DistinctNames2.Jumper_PoloNeck",
+  "DistinctNames2.Jumper_RoundNeck",
+  "DistinctNames2.Jumper_VNeck",
+  "DistinctNames2.Jumper_TankTopTINT",
+}
+local listSweaterHat = {
+  "DistinctNames2.HoodieUP_WhiteTINT",
+}
+local listJacket = {
+  "DistinctNames2.Jacket_CoatArmy",
+  "DistinctNames2.Jacket_ArmyCamoGreen",
+  "DistinctNames2.Jacket_ArmyCamoDesert",
+  "DistinctNames2.Jacket_Chef",
+  "DistinctNames2.Jacket_Fireman",
+  "DistinctNames2.Jacket_Black",
+  "DistinctNames2.JacketLeatherColor_Short",
+  "DistinctNames2.JacketLong_Random",
+  "DistinctNames2.JacketLeatherColor_Long",
+  "DistinctNames2.JacketLong_Doctor",
+  "DistinctNames2.Jacket_PaddedDOWN",
+  "DistinctNames2.PonchoYellowDOWN",
+  "DistinctNames2.Jacket_Police",
+  "DistinctNames2.Jacket_Ranger",
+  "DistinctNames2.JacketLong_Santa",
+  "DistinctNames2.JacketLong_SantaGreen",
+  "DistinctNames2.Suit_Jacket",
+  "DistinctNames2.WeddingJacket",
+  "DistinctNames2.Jacket_Varsity",
+  "DistinctNames2.Jacket_Varsity2",
+}
+local listJacketHat = {
+  "DistinctNames2.Jacket_Padded",
+  "DistinctNames2.PonchoYellow",
+}
+local listFullSuit = {
+  "DistinctNames2.Boilersuit",
+  "DistinctNames2.Boilersuit_Prisoner",
+  "DistinctNames2.Boilersuit_PrisonerKhaki",
+  "DistinctNames2.SpiffoSuit",
+  "DistinctNames2.WeddingDress",
+}
+local listFullSuitHead = {
+  "DistinctNames2.HazmatSuit",
+}
+local listFullTop = {
+  "DistinctNames2.Ghillie_Top",
+}
+local listBathRobe = {
+  "DistinctNames2.LongCoat_Bathrobe",
+}
+local listTorsoExtra = {
+  "DistinctNames2.Apron_White",
+  "DistinctNames2.Apron_Black",
+  "DistinctNames2.Apron_WhiteTEXTURE",
+  "DistinctNames2.Apron_IceCream",
+  "DistinctNames2.Apron_PileOCrepe",
+  "DistinctNames2.Apron_PizzaWhirled",
+  "DistinctNames2.Apron_Spiffos",
+  "DistinctNames2.Vest_BulletCivilian",
+  "DistinctNames2.Vest_BulletPolice",
+  "DistinctNames2.Vest_BulletArmy",
+  "DistinctNames2.Vest_Waistcoat",
+  "DistinctNames2.Vest_Waistcoat_GigaMart",
+  "DistinctNames2.Vest_Foreman",
+  "DistinctNames2.Vest_HighViz",
+  "DistinctNames2.Vest_Hunting_Grey",
+  "DistinctNames2.Vest_Hunting_Orange",
+  "DistinctNames2.Vest_Hunting_Camo",
+  "DistinctNames2.Vest_Hunting_CamoGreen",
+}
+local listTail = {
+  "DistinctNames2.SpiffoTail",
+}
+local listBack = {
+  "Base.Bag_ALICEpack_Army",
+  "Base.Bag_ALICEpack",
+  "Base.Bag_BigHikingBag",
+  "Base.Bag_NormalHikingBag",
+  "Base.Bag_DuffelBagTINT",
+  "Base.Bag_Schoolbag",
+}
+local listScarf = {
+  "DistinctNames2.Scarf_White",
+  "DistinctNames2.Scarf_StripeBlackWhite",
+}
+
+
+
+ClothingSelectionDefinitions.default.Male.Hat = {chance = 10, items = listHat}
+ClothingSelectionDefinitions.default.Male.TankTop = {chance = 10, items = listTankTop}
+ClothingSelectionDefinitions.default.Male.Tshirt = {items = listTshirt}
+ClothingSelectionDefinitions.default.Male.Shirt = {chance = 10, items = listShirt}
+ClothingSelectionDefinitions.default.Male.Socks = {chance = 0, items = listSocks}
+ClothingSelectionDefinitions.default.Male.Pants = {items = listPants}
+ClothingSelectionDefinitions.default.Male.Skirt = {chance = 0, items = listSkirt}
+ClothingSelectionDefinitions.default.Male.Dress = {chance = 0, items = listDress}
+ClothingSelectionDefinitions.default.Male.Shoes = {items = listShoes}
+ClothingSelectionDefinitions.default.Male.Eyes = {chance = 10, items = listEyes}
+ClothingSelectionDefinitions.default.Male.BeltExtra = {chance = 0, items = listBeltExtra}
+ClothingSelectionDefinitions.default.Male.Mask = {chance = 0, items = listMask}
+ClothingSelectionDefinitions.default.Male.MaskEyes = {chance = 0, items = listMaskEyes}
+ClothingSelectionDefinitions.default.Male.Underwear = {chance = 0, items = listUnderwear}
+ClothingSelectionDefinitions.default.Male.FullHat = {chance = 0, items = listFullHat}
+ClothingSelectionDefinitions.default.Male.Torso1Legs1 = {chance = 0, items = listTorso1Legs1}
+ClothingSelectionDefinitions.default.Male.Neck = {chance = 0, items = listNeck}
+ClothingSelectionDefinitions.default.Male.Hands = {chance = 0, items = listHands}
+ClothingSelectionDefinitions.default.Male.Legs1 = {chance = 0, items = listLegs1}
+ClothingSelectionDefinitions.default.Male.Sweater = {chance = 30, items = listSweater}
+ClothingSelectionDefinitions.default.Male.Jacket = {chance = 30, items = listJacket}
+ClothingSelectionDefinitions.default.Male.FullSuit = {chance = 0, items = listFullSuit}
+ClothingSelectionDefinitions.default.Male.FullSuitHead = {chance = 0, items = listFullSuitHead}
+ClothingSelectionDefinitions.default.Male.FullTop = {chance = 0, items = listFullTop}
+ClothingSelectionDefinitions.default.Male.BathRobe = {chance = 0, items = listBathRobe}
+ClothingSelectionDefinitions.default.Male.TorsoExtra = {chance = 0, items = listTorsoExtra}
+ClothingSelectionDefinitions.default.Male.Tail = {chance = 0, items = listTail}
+ClothingSelectionDefinitions.default.Male.Back = {chance = 0, items = listBack}
+ClothingSelectionDefinitions.default.Male.Scarf = {chance = 0, items = listScarf}
+
+
+ClothingSelectionDefinitions.default.Female.Hat = {chance = 10, items = listHat}
+ClothingSelectionDefinitions.default.Female.TankTop = {chance = 10, items = listTankTop}
+ClothingSelectionDefinitions.default.Female.Tshirt = {items = listTshirt}
+ClothingSelectionDefinitions.default.Female.Shirt = {chance = 10, items = listShirt}
+ClothingSelectionDefinitions.default.Female.Socks = {chance = 0, items = listSocks}
+ClothingSelectionDefinitions.default.Female.Pants = {items = listPants}
+ClothingSelectionDefinitions.default.Female.Skirt = {chance = 0, items = listSkirt}
+ClothingSelectionDefinitions.default.Female.Dress = {chance = 0, items = listDress}
+ClothingSelectionDefinitions.default.Female.Shoes = {items = listShoes}
+ClothingSelectionDefinitions.default.Female.Eyes = {chance = 10, items = listEyes}
+ClothingSelectionDefinitions.default.Female.BeltExtra = {chance = 0, items = listBeltExtra}
+ClothingSelectionDefinitions.default.Female.Mask = {chance = 0, items = listMask}
+ClothingSelectionDefinitions.default.Female.MaskEyes = {chance = 0, items = listMaskEyes}
+ClothingSelectionDefinitions.default.Female.Underwear = {chance = 0, items = listUnderwear}
+ClothingSelectionDefinitions.default.Female.FullHat = {chance = 0, items = listFullHat}
+ClothingSelectionDefinitions.default.Female.Torso1Legs1 = {chance = 0, items = listTorso1Legs1}
+ClothingSelectionDefinitions.default.Female.Neck = {chance = 0, items = listNeck}
+ClothingSelectionDefinitions.default.Female.Hands = {chance = 0, items = listHands}
+ClothingSelectionDefinitions.default.Female.Legs1 = {chance = 0, items = listLegs1}
+ClothingSelectionDefinitions.default.Female.Sweater = {chance = 30, items = listSweater}
+ClothingSelectionDefinitions.default.Female.Jacket = {chance = 30, items = listJacket}
+ClothingSelectionDefinitions.default.Female.FullSuit = {chance = 0, items = listFullSuit}
+ClothingSelectionDefinitions.default.Female.FullSuitHead = {chance = 0, items = listFullSuitHead}
+ClothingSelectionDefinitions.default.Female.FullTop = {chance = 0, items = listFullTop}
+ClothingSelectionDefinitions.default.Female.BathRobe = {chance = 0, items = listBathRobe}
+ClothingSelectionDefinitions.default.Female.TorsoExtra = {chance = 0, items = listTorsoExtra}
+ClothingSelectionDefinitions.default.Female.Tail = {chance = 0, items = listTail}
+ClothingSelectionDefinitions.default.Female.Back = {chance = 0, items = listBack}
+ClothingSelectionDefinitions.default.Female.Scarf = {chance = 0, items = listScarf}
+
+
+-- startingOutfit --
+ClothingSelectionDefinitions.starting =ClothingSelectionDefinitions.starting or {}
+ClothingSelectionDefinitions.starting.Male = ClothingSelectionDefinitions.starting.Male or {}
+ClothingSelectionDefinitions.starting.Female = ClothingSelectionDefinitions.starting.Female or {}
+ClothingSelectionDefinitions.starting.Male.Shoes = {shance=100, items = {"DistinctNames2.Shoes_TrainerTINT"}}
+ClothingSelectionDefinitions.starting.Male.TankTop = {chance=100, items = {"DistinctNames2.Vest_DefaultTEXTURE_TINT"}}
+ClothingSelectionDefinitions.starting.Male.Socks = {chance=100, items = {"DistinctNames2.Socks_Ankle"}}
+ClothingSelectionDefinitions.starting.Male.Pants = {chance=100, items = {"DistinctNames2.Trousers_WhiteTINT"}}
+
+ClothingSelectionDefinitions.starting.Female.Shoes = {shance=100, items = {"DistinctNames2.Shoes_TrainerTINT"}}
+ClothingSelectionDefinitions.starting.Female.TankTop = {chance=100, items = {"DistinctNames2.Vest_DefaultTEXTURE_TINT"}}
+ClothingSelectionDefinitions.starting.Female.Socks = {chance=100, items = {"DistinctNames2.Socks_Ankle"}}
+ClothingSelectionDefinitions.starting.Female.Pants = {chance=100, items = {"DistinctNames2.Trousers_WhiteTINT"}}
