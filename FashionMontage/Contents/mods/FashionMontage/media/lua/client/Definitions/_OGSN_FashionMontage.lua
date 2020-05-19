9e820ba217cab4e5aee6a52139fd44029211caf0
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-19 02:01:52 -0400

    The mod now has two lua files, a base one that sets everything up and defines the addClothing function, and another which just adds the clothing using that pre-defined function

--- FashionMontage/Contents/mods/FashionMontage/media/lua/client/Definitions/_OGSN_FashionMontage.lua
+++ FashionMontage/Contents/mods/FashionMontage/media/lua/client/Definitions/_OGSN_FashionMontage.lua
@@ -1,9 +1,11 @@
 -- NOTE TO MODDERS: Do NOT overwrite this file.
 -- If you want to add new clothing, create a new .lua file using the
--- template I have created in media/lua/client/Definitions/items_ExampleModName.lua
+-- template I have created in media/lua/client/Definitions/modClothing_ExampleModName.lua
 
 -- This file creates empty dropdowns for all the possible BodyParts
 -- The other lua files in this folder add items to those dropdowns
+
+-- Create a blank slate of empty dropdowns for Male and Female
 ClothingSelectionDefinitions.default.Male.Hat = {}
 ClothingSelectionDefinitions.default.Male.Hat.items = {}
 ClothingSelectionDefinitions.default.Male.Hat.chance = 20
@@ -200,7 +202,7 @@ ClothingSelectionDefinitions.default.Male.Skirt = {}
 ClothingSelectionDefinitions.default.Male.Skirt.items = {}
 ClothingSelectionDefinitions.default.Male.Skirt.chance = 0
 
--- startingOutfit --
+-- Make the default starting outfit something really basic and easy for the player to replace
 ClothingSelectionDefinitions.starting = {}
 ClothingSelectionDefinitions.starting.Male = {}
 ClothingSelectionDefinitions.starting.Female = {}
@@ -214,3 +216,268 @@ ClothingSelectionDefinitions.starting.Female.Shoes = {chance=100, items = {"Dist
 ClothingSelectionDefinitions.starting.Female.TankTop = {chance=100, items = {"DistinctNames2.Vest_DefaultTEXTURE_TINT"}}
 ClothingSelectionDefinitions.starting.Female.Socks = {chance=100, items = {"DistinctNames2.Socks_Ankle"}}
 ClothingSelectionDefinitions.starting.Female.Pants = {chance=100, items = {"DistinctNames2.Trousers_WhiteTINT"}}
+
+-- Define the function that will add lists of clothing, vanilla or modded, to the dropdowns
+local function addClothing(clothingLists)
+
+    local function merge_Old_New(old,new)
+      for k,v in pairs(new) do old[k] = v end;
+    end
+
+    local maleTable = ClothingSelectionDefinitions.default.Male
+    local femaleTable = ClothingSelectionDefinitions.default.Female
+
+    if clothingLists.listHat then
+      maleTable.Hat.chance = 20;
+      femaleTable.Hat.chance = 20;
+      local maleItems = maleTable.Hat.items;
+      local femaleItems = femaleTable.Hat.items;
+      merge_Old_New(maleItems,clothingLists.listHat);
+      merge_Old_New(femaleItems,clothingLists.listHat);
+    end
+    if clothingLists.listTankTop then
+      maleTable.TankTop.chance = 20;
+      femaleTable.TankTop.chance = 20;
+      local maleItems = maleTable.TankTop.items;
+      local femaleItems = femaleTable.TankTop.items;
+      merge_Old_New(maleItems,clothingLists.listTankTop);
+      merge_Old_New(femaleItems,clothingLists.listTankTop);
+    end
+    if clothingLists.listTshirt then
+      maleTable.Tshirt.chance = 20;
+      femaleTable.Tshirt.chance = 20;
+      local maleItems = maleTable.Tshirt.items;
+      local femaleItems = femaleTable.Tshirt.items;
+      merge_Old_New(maleItems,clothingLists.listTshirt);
+      merge_Old_New(femaleItems,clothingLists.listTshirt);
+    end
+    if clothingLists.listShirt then
+      maleTable.Shirt.chance = 20;
+      femaleTable.Shirt.chance = 20;
+      local maleItems = maleTable.Shirt.items;
+      local femaleItems = femaleTable.Shirt.items;
+      merge_Old_New(maleItems,clothingLists.listShirt);
+      merge_Old_New(femaleItems,clothingLists.listShirt);
+    end
+    if clothingLists.listSocks then
+      maleTable.Socks.chance = 20;
+      femaleTable.Socks.chance = 20;
+      local maleItems = maleTable.Socks.items;
+      local femaleItems = femaleTable.Socks.items;
+      merge_Old_New(maleItems,clothingLists.listSocks);
+      merge_Old_New(femaleItems,clothingLists.listSocks);
+    end
+    if clothingLists.listPants then
+      maleTable.Pants.chance = 20;
+      femaleTable.Pants.chance = 20;
+      local maleItems = maleTable.Pants.items;
+      local femaleItems = femaleTable.Pants.items;
+      merge_Old_New(maleItems,clothingLists.listPants);
+      merge_Old_New(femaleItems,clothingLists.listPants);
+    end
+    if clothingLists.listSkirt then
+      maleTable.Skirt.chance = 0;
+      femaleTable.Skirt.chance = 20;
+      local maleItems = maleTable.Skirt.items;
+      local femaleItems = femaleTable.Skirt.items;
+      merge_Old_New(maleItems,clothingLists.listSkirt);
+      merge_Old_New(femaleItems,clothingLists.listSkirt);
+    end
+    if clothingLists.listDress then
+      maleTable.Dress.chance = 0;
+      femaleTable.Dress.chance = 20;
+      local maleItems = maleTable.Dress.items;
+      local femaleItems = femaleTable.Dress.items;
+      merge_Old_New(maleItems,clothingLists.listDress);
+      merge_Old_New(femaleItems,clothingLists.listDress);
+    end
+    if clothingLists.listShoes then
+      maleTable.Shoes.chance = 20;
+      femaleTable.Shoes.chance = 20;
+      local maleItems = maleTable.Shoes.items;
+      local femaleItems = femaleTable.Shoes.items;
+      merge_Old_New(maleItems,clothingLists.listShoes);
+      merge_Old_New(femaleItems,clothingLists.listShoes);
+    end
+    if clothingLists.listEyes then
+      maleTable.Eyes.chance = 20;
+      femaleTable.Eyes.chance = 20;
+      local maleItems = maleTable.Eyes.items;
+      local femaleItems = femaleTable.Eyes.items;
+      merge_Old_New(maleItems,clothingLists.listEyes);
+      merge_Old_New(femaleItems,clothingLists.listEyes);
+    end
+    if clothingLists.listBeltExtra then
+      maleTable.BeltExtra.chance = 20;
+      femaleTable.BeltExtra.chance = 20;
+      local maleItems = maleTable.BeltExtra.items;
+      local femaleItems = femaleTable.BeltExtra.items;
+      merge_Old_New(maleItems,clothingLists.listBeltExtra);
+      merge_Old_New(femaleItems,clothingLists.listBeltExtra);
+    end
+    if clothingLists.listMask then
+      maleTable.Mask.chance = 20;
+      femaleTable.Mask.chance = 20;
+      local maleItems = maleTable.Mask.items;
+      local femaleItems = femaleTable.Mask.items;
+      merge_Old_New(maleItems,clothingLists.listMask);
+      merge_Old_New(femaleItems,clothingLists.listMask);
+    end
+    if clothingLists.listMaskEyes then
+      maleTable.MaskEyes.chance = 20;
+      femaleTable.MaskEyes.chance = 20;
+      local maleItems = maleTable.MaskEyes.items;
+      local femaleItems = femaleTable.MaskEyes.items;
+      merge_Old_New(maleItems,clothingLists.listMaskEyes);
+      merge_Old_New(femaleItems,clothingLists.listMaskEyes);
+    end
+    if clothingLists.listUnderwear then
+      maleTable.Underwear.chance = 20;
+      femaleTable.Underwear.chance = 20;
+      local maleItems = maleTable.Underwear.items;
+      local femaleItems = femaleTable.Underwear.items;
+      merge_Old_New(maleItems,clothingLists.listUnderwear);
+      merge_Old_New(femaleItems,clothingLists.listUnderwear);
+    end
+    if clothingLists.listFullHat then
+      maleTable.FullHat.chance = 20;
+      femaleTable.FullHat.chance = 20;
+      local maleItems = maleTable.FullHat.items;
+      local femaleItems = femaleTable.FullHat.items;
+      merge_Old_New(maleItems,clothingLists.listFullHat);
+      merge_Old_New(femaleItems,clothingLists.listFullHat);
+    end
+    if clothingLists.listTorso1Legs1 then
+      maleTable.Torso1Legs1.chance = 20;
+      femaleTable.Torso1Legs1.chance = 20;
+      local maleItems = maleTable.Torso1Legs1.items;
+      local femaleItems = femaleTable.Torso1Legs1.items;
+      merge_Old_New(maleItems,clothingLists.listTorso1Legs1);
+      merge_Old_New(femaleItems,clothingLists.listTorso1Legs1);
+    end
+    if clothingLists.listNeck then
+      maleTable.Neck.chance = 20;
+      femaleTable.Neck.chance = 20;
+      local maleItems = maleTable.Neck.items;
+      local femaleItems = femaleTable.Neck.items;
+      merge_Old_New(maleItems,clothingLists.listNeck);
+      merge_Old_New(femaleItems,clothingLists.listNeck);
+    end
+    if clothingLists.listHands then
+      maleTable.Hands.chance = 20;
+      femaleTable.Hands.chance = 20;
+      local maleItems = maleTable.Hands.items;
+      local femaleItems = femaleTable.Hands.items;
+      merge_Old_New(maleItems,clothingLists.listHands);
+      merge_Old_New(femaleItems,clothingLists.listHands);
+    end
+    if clothingLists.listLegs1 then
+      maleTable.Legs1.chance = 20;
+      femaleTable.Legs1.chance = 20;
+      local maleItems = maleTable.Legs1.items;
+      local femaleItems = femaleTable.Legs1.items;
+      merge_Old_New(maleItems,clothingLists.listLegs1);
+      merge_Old_New(femaleItems,clothingLists.listLegs1);
+    end
+    if clothingLists.listSweater then
+      maleTable.Sweater.chance = 20;
+      femaleTable.Sweater.chance = 20;
+      local maleItems = maleTable.Sweater.items;
+      local femaleItems = femaleTable.Sweater.items;
+      merge_Old_New(maleItems,clothingLists.listSweater);
+      merge_Old_New(femaleItems,clothingLists.listSweater);
+    end
+    if clothingLists.listSweaterHat then
+      maleTable.SweaterHat.chance = 20;
+      femaleTable.SweaterHat.chance = 20;
+      local maleItems = maleTable.SweaterHat.items;
+      local femaleItems = femaleTable.SweaterHat.items;
+      merge_Old_New(maleItems,clothingLists.listSweaterHat);
+      merge_Old_New(femaleItems,clothingLists.listSweaterHat);
+    end
+    if clothingLists.listJacket then
+      maleTable.Jacket.chance = 20;
+      femaleTable.Jacket.chance = 20;
+      local maleItems = maleTable.Jacket.items;
+      local femaleItems = femaleTable.Jacket.items;
+      merge_Old_New(maleItems,clothingLists.listJacket);
+      merge_Old_New(femaleItems,clothingLists.listJacket);
+    end
+    if clothingLists.listJacketHat then
+      maleTable.JacketHat.chance = 20;
+      femaleTable.JacketHat.chance = 20;
+      local maleItems = maleTable.JacketHat.items;
+      local femaleItems = femaleTable.JacketHat.items;
+      merge_Old_New(maleItems,clothingLists.listJacketHat);
+      merge_Old_New(femaleItems,clothingLists.listJacketHat);
+    end
+    if clothingLists.listFullSuit then
+      maleTable.FullSuit.chance = 20;
+      femaleTable.FullSuit.chance = 20;
+      local maleItems = maleTable.FullSuit.items;
+      local femaleItems = femaleTable.FullSuit.items;
+      merge_Old_New(maleItems,clothingLists.listFullSuit);
+      merge_Old_New(femaleItems,clothingLists.listFullSuit);
+    end
+    if clothingLists.listFullSuitHead then
+      maleTable.FullSuitHead.chance = 20;
+      femaleTable.FullSuitHead.chance = 20;
+      local maleItems = maleTable.FullSuitHead.items;
+      local femaleItems = femaleTable.FullSuitHead.items;
+      merge_Old_New(maleItems,clothingLists.listFullSuitHead);
+      merge_Old_New(femaleItems,clothingLists.listFullSuitHead);
+    end
+    if clothingLists.listFullTop then
+      maleTable.FullTop.chance = 20;
+      femaleTable.FullTop.chance = 20;
+      local maleItems = maleTable.FullTop.items;
+      local femaleItems = femaleTable.FullTop.items;
+      merge_Old_New(maleItems,clothingLists.listFullTop);
+      merge_Old_New(femaleItems,clothingLists.listFullTop);
+    end
+    if clothingLists.listBathRobe then
+      maleTable.BathRobe.chance = 20;
+      femaleTable.BathRobe.chance = 20;
+      local maleItems = maleTable.BathRobe.items;
+      local femaleItems = femaleTable.BathRobe.items;
+      merge_Old_New(maleItems,clothingLists.listBathRobe);
+      merge_Old_New(femaleItems,clothingLists.listBathRobe);
+    end
+    if clothingLists.listTorsoExtra then
+      maleTable.TorsoExtra.chance = 20;
+      femaleTable.TorsoExtra.chance = 20;
+      local maleItems = maleTable.TorsoExtra.items;
+      local femaleItems = femaleTable.TorsoExtra.items;
+      merge_Old_New(maleItems,clothingLists.listTorsoExtra);
+      merge_Old_New(femaleItems,clothingLists.listTorsoExtra);
+    end
+    if clothingLists.listTail then
+      maleTable.Tail.chance = 20;
+      femaleTable.Tail.chance = 20;
+      local maleItems = maleTable.Tail.items;
+      local femaleItems = femaleTable.Tail.items;
+      merge_Old_New(maleItems,clothingLists.listTail);
+      merge_Old_New(femaleItems,clothingLists.listTail);
+    end
+    if clothingLists.listBack then
+      maleTable.Back.chance = 20;
+      femaleTable.Back.chance = 20;
+      local maleItems = maleTable.Back.items;
+      local femaleItems = femaleTable.Back.items;
+      merge_Old_New(maleItems,clothingLists.listBack);
+      merge_Old_New(femaleItems,clothingLists.listBack);
+    end
+    if clothingLists.listScarf then
+      maleTable.Scarf.chance = 20;
+      femaleTable.Scarf.chance = 20;
+      local maleItems = maleTable.Scarf.items;
+      local femaleItems = femaleTable.Scarf.items;
+      merge_Old_New(maleItems,clothingLists.listScarf);
+      merge_Old_New(femaleItems,clothingLists.listScarf);
+    end
+end
+
+-- Make this function available to other lua files that will add clothing to the dropdowns
+_OGSN_FashionMontage = {}
+_OGSN_FashionMontage.addClothing = addClothing
+return _OGSN_FashionMontage
