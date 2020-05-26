fbad44f4012335d52d1c42d924891c52c3a554d1
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-05-26 17:21:41 -0400

    Adjusts (fixes?) the vehiclezonedistributions in Mystery Machine

--- MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
+++ MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
@@ -1,12 +1,10 @@
-if VehicleZoneDistribution then -- check if the table exists for backwards compatibility
+VehicleZoneDistribution = VehicleZoneDistribution or {};
 
-  VehicleZoneDistribution.police.chanceToSpawnNormal = 70;
-  VehicleZoneDistribution.fire.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
-  VehicleZoneDistribution.ranger.vehicles["Base.MysteryMachine"] = {index = 0, spawnChance = 1};
-  VehicleZoneDistribution.mccoy.vehicles["Base.MysteryMachine"] = {index = 1, spawnChance = 1};
-  VehicleZoneDistribution.postal.vehicles["Base.MysteryMachine"] = {index = 2, spawnChance = 1};
-  VehicleZoneDistribution.spiffo.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
-  VehicleZoneDistribution.ambulance.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
-  VehicleZoneDistribution.radio.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
-  VehicleZoneDistribution.fossoil.vehicles["Base.MysteryMachine"] = {index = 0, spawnChance = 1};
-end
+VehicleZoneDistribution.fire.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.ranger.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.mccoy.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.postal.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.spiffo.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.ambulance.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.radio.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.fossoil.vehicles["Base.MysteryMachine"] = {index = -1, spawnChance = 1};
