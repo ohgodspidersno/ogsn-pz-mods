bad4452cf47eb1b2e8847088c638bc76a7f36534
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-08-29 20:53:13 -0400

    Updates Fashion Montage to 41.41 and changes how it works so that it adjusts the names of all items instead of making new copies

---
+++ FashionMontage/Contents/mods/FashionMontage/media/lua/client/DisplayNameTweaks/tweaks_FMogsn_dresses.lua
@@ -0,0 +1,9 @@
+require("ItemTweaker_Copy_ogsn");
+-- Some of these aren't necessary but I will do them all anyway
+-- TweakItem("Base.Belt2","Displayname","Belt");
+
+TweakItem("Base.Dress_Long","DisplayName","Dress - Ankle");
+TweakItem("Base.Dress_Normal","DisplayName","Dress - Calf");
+TweakItem("Base.Dress_Knees","DisplayName","Dress - Knee");
+TweakItem("Base.Dress_Short","DisplayName","Dress - Thigh");
+TweakItem("Base.HospitalGown","DisplayName","Hospital Gown");
---
+++ FashionMontage/Contents/mods/FashionMontage/media/lua/client/DisplayNameTweaks/tweaks_FMogsn_jacket_hat.lua
@@ -0,0 +1,7 @@
+require("ItemTweaker_Copy_ogsn");
+-- TweakItem("Module.ItemName","AttributeName","New Attribute Value");
+
+-- Some of these aren't necessary but I will do them all anyway
+TweakItem("Base.Jacket_Padded","DisplayName","Padded Jacket");
+TweakItem("Base.PonchoGreen","DisplayName","Poncho - Green");
+TweakItem("Base.PonchoYellow","DisplayName","Poncho - Yellow");
