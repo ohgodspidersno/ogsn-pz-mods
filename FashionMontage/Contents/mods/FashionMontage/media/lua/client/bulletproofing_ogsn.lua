9cf70c926ed6e4710c2bd16aecdcb7507d5cf530
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-08-31 15:02:37 -0400

    Typos

--- FashionMontage/Contents/mods/FashionMontage/media/lua/client/bulletproofing_ogsn.lua
+++ FashionMontage/Contents/mods/FashionMontage/media/lua/client/bulletproofing_ogsn.lua
@@ -1,42 +1,32 @@
 require("ItemTweaker_Copy_ogsn");
 if getActivatedMods():contains("ArmoredVests") then
-  -- TweakItem("Base.Vest_BulletArmy","DisplayName","Armored Military Bulletproof Vest")
-  TweakItem("Base.Vest_BulletArmy","BiteDefense",100)
-  TweakItem("Base.Vest_BulletArmy","ScratchDefense",100)
+  TweakItem("Base.Vest_BulletArmy","BiteDefense","100");
+  TweakItem("Base.Vest_BulletArmy","ScratchDefense","100");
 
-  -- TweakItem("Base.Vest_BulletPolice","DisplayName","Armored Police Bulletproof Vest")
-  TweakItem("Base.Vest_BulletPolice","BiteDefense",100)
-  TweakItem("Base.Vest_BulletPolice","ScratchDefense",100)
+  TweakItem("Base.Vest_BulletPolice","BiteDefense","100");
+  TweakItem("Base.Vest_BulletPolice","ScratchDefense","100");
 
-  -- TweakItem("Base.Vest_BulletCivilian","DisplayName","Armored Civilian Bulletproof Vest")
-  TweakItem("Base.Vest_BulletCivilian","BiteDefense",10)
-  TweakItem("Base.Vest_BulletCivilian","ScratchDefense",100,)
+  TweakItem("Base.Vest_BulletCivilian","BiteDefense","10");
+  TweakItem("Base.Vest_BulletCivilian","ScratchDefense","100");
 
-  -- TweakItem("Base.Boilersuit_Flying","DisplayName","Airforce Coveralls")
-  TweakItem("Base.Boilersuit_Flying","BiteDefense",30)
-  TweakItem("Base.Boilersuit_Flying","ScratchDefense",30)
+  TweakItem("Base.Boilersuit_Flying","BiteDefense","30");
+  TweakItem("Base.Boilersuit_Flying","ScratchDefense","30");
 
-  -- TweakItem("Base.HazmatSuit","DisplayName","Hazmat Suit")
-  TweakItem("Base.HazmatSuit","BiteDefense",65)
-  TweakItem("Base.HazmatSuit","ScratchDefense",65)
+  TweakItem("Base.HazmatSuit","BiteDefense","65");
+  TweakItem("Base.HazmatSuit","ScratchDefense","65");
 
-  -- TweakItem("Base.Hat_NBCmask","DisplayName","Nuclear Biochemical Mask")
-  TweakItem("Base.Hat_NBCmask","BiteDefense",75)
-  TweakItem("Base.Hat_NBCmask","ScratchDefense",75)
+  TweakItem("Base.Hat_NBCmask","BiteDefense","75");
+  TweakItem("Base.Hat_NBCmask","ScratchDefense","75");
 
-  -- TweakItem("Base.Hat_SPHhelmet","DisplayName","Airforce Helmet")
-  TweakItem("Base.Hat_SPHhelmet","BiteDefense",100)
-  TweakItem("Base.Hat_SPHhelmet","ScratchDefense",100)
+  TweakItem("Base.Hat_SPHhelmet","BiteDefense","100");
+  TweakItem("Base.Hat_SPHhelmet","ScratchDefense","100");
 
-  -- TweakItem("Base.Hat_GasMask","DisplayName","Gas Mask")
-  TweakItem("Base.Hat_GasMask","BiteDefense",35)
-  TweakItem("Base.Hat_GasMask","ScratchDefense",35)
+  TweakItem("Base.Hat_GasMask","BiteDefense","35");
+  TweakItem("Base.Hat_GasMask","ScratchDefense","35");
 
-  -- TweakItem("Base.Hat_BalaclavaFace","DisplayName","Balaclava - Open")
-  TweakItem("Base.Hat_BalaclavaFace","BiteDefense",20)
-  TweakItem("Base.Hat_BalaclavaFace","ScratchDefense",20)
+  TweakItem("Base.Hat_BalaclavaFace","BiteDefense","20");
+  TweakItem("Base.Hat_BalaclavaFace","ScratchDefense","20");
 
-  -- TweakItem("Base.Hat_BalaclavaFull","DisplayName","Balaclava")
-  TweakItem("Base.Hat_BalaclavaFull","BiteDefense",20)
-  TweakItem("Base.Hat_BalaclavaFull","ScratchDefense",20)
+  TweakItem("Base.Hat_BalaclavaFull","BiteDefense","20");
+  TweakItem("Base.Hat_BalaclavaFull","ScratchDefense","20");
 end
