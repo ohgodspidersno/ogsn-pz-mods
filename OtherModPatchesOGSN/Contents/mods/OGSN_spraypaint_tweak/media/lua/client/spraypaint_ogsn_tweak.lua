11722ef60a0749da5b5d2d326a8344db9289d181
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-10-09 18:14:05 -0400

    SprayPaint Tweak - Fixes missing module name

--- OtherModPatchesOGSN/Contents/mods/OGSN_spraypaint_tweak/media/lua/client/spraypaint_ogsn_tweak.lua
+++ OtherModPatchesOGSN/Contents/mods/OGSN_spraypaint_tweak/media/lua/client/spraypaint_ogsn_tweak.lua
@@ -1,21 +1,19 @@
 require("ItemTweaker_Copy_ogsn");
-if getActivatedMods():contains("spraypaint") then
-  local new_weight = "0.1"
-  TweakItem("SpraycanWhite","Weight",new_weight);
-  TweakItem("SpraycanRed","Weight",new_weight);
-  TweakItem("SpraycanBlue","Weight",new_weight);
-  TweakItem("SpraycanGreen","Weight",new_weight);
-  TweakItem("SpraycanOrange","Weight",new_weight);
-  TweakItem("SpraycanYellow","Weight",new_weight);
-  TweakItem("SpraycanViolet","Weight",new_weight);
-  TweakItem("SpraycanBlack","Weight",new_weight);
-  TweakItem("SpraycanCyan","Weight",new_weight);
-  TweakItem("ChalkWhite","Weight",new_weight);
-  TweakItem("ChalkRed","Weight",new_weight);
-  TweakItem("ChalkBlue","Weight",new_weight);
-  TweakItem("ChalkGreen","Weight",new_weight);
-  TweakItem("ChalkOrange","Weight",new_weight);
-  TweakItem("ChalkYellow","Weight",new_weight);
-  TweakItem("ChalkViolet","Weight",new_weight);
-  TweakItem("ChalkCyan","Weight",new_weight);
-end
+local new_weight = "0.1"
+TweakItem("spraypaint.SpraycanWhite","Weight",new_weight);
+TweakItem("spraypaint.SpraycanRed","Weight",new_weight);
+TweakItem("spraypaint.SpraycanBlue","Weight",new_weight);
+TweakItem("spraypaint.SpraycanGreen","Weight",new_weight);
+TweakItem("spraypaint.SpraycanOrange","Weight",new_weight);
+TweakItem("spraypaint.SpraycanYellow","Weight",new_weight);
+TweakItem("spraypaint.SpraycanViolet","Weight",new_weight);
+TweakItem("spraypaint.SpraycanBlack","Weight",new_weight);
+TweakItem("spraypaint.SpraycanCyan","Weight",new_weight);
+TweakItem("spraypaint.ChalkWhite","Weight",new_weight);
+TweakItem("spraypaint.ChalkRed","Weight",new_weight);
+TweakItem("spraypaint.ChalkBlue","Weight",new_weight);
+TweakItem("spraypaint.ChalkGreen","Weight",new_weight);
+TweakItem("spraypaint.ChalkOrange","Weight",new_weight);
+TweakItem("spraypaint.ChalkYellow","Weight",new_weight);
+TweakItem("spraypaint.ChalkViolet","Weight",new_weight);
+TweakItem("spraypaint.ChalkCyan","Weight",new_weight);
