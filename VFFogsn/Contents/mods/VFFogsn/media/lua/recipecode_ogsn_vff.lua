733fa3838bc0f9594654882b2a09b867666d8b9a
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-17 21:13:35 -0500

    VFF - Adds Give1CookingXP recipecode

---
+++ VFFogsn/Contents/mods/VFFogsn/media/lua/recipecode_ogsn_vff.lua
@@ -0,0 +1,11 @@
+Recipe = Recipe or {}
+Recipe.GetItemTypes = Recipe.GetItemTypes or {}
+Recipe.OnCreate = Recipe.OnCreate or {}
+Recipe.OnGiveXP = Recipe.OnGiveXP or {}
+Recipe.OnTest = Recipe.OnTest or {}
+
+function Recipe.OnGiveXP.Cooking1(recipe, ingredients, result, player)
+    player:getXp():AddXP(Perks.Cooking, 1);
+end
+
+Give1CookingXP = Recipe.OnGiveXP.Cooking1
