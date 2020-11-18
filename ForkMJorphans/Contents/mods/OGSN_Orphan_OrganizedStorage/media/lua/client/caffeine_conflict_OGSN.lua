ccecc275fe85b3d15552fd32b3c25d4ed4ebc36e
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-17 20:38:51 -0500

    WIP tweakrecipe name (adds _ underscores)

--- ForkMJorphans/Contents/mods/OGSN_Orphan_OrganizedStorage/media/lua/client/caffeine_conflict_OGSN.lua
+++ ForkMJorphans/Contents/mods/OGSN_Orphan_OrganizedStorage/media/lua/client/caffeine_conflict_OGSN.lua
@@ -1,4 +1,4 @@
 require("RecipeTweaker_Copy_ogsn");
 if getActivatedMods():contains("OGSN_orphan_GoodTeaCoffeeVitamins") or getActivatedMods():contains("GoodTeaCoffeeVitamins") then
-  
+  TweakRecipe("Base.Unpack_Instant_Coffee_Mix","Coffee2","100");
 end
