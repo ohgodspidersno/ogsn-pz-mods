30a13e5792539f23a4980d691f0e0ad03a2de305
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-21 14:17:49 -0500

    General - Removes mods I no longer intend on working on

--- InfiniteLamps/Contents/mods/InfiniteLamps/media/lua/server/InfiniteLampPillar.lua
+++
@@ -1,7 +0,0 @@
-local ISLightSource_create_original = ISLightSource.create
-
-function ISLightSource:create(x, y, z, north, sprite, ...)
-	ISLightSource_create_original(self, x, y, z, north, sprite, ...)
-	self.javaObject:setLifeDelta(0);
-	-- return result -- not sure if this is necessary
-end
