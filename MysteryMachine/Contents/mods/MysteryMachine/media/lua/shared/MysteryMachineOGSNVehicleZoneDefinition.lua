03c188f0154a62101442228705c3c6dd860a1b41
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-21 22:24:13 -0500

    Mystery Machine - Fixes inconsistent name capitalization causing problems

--- MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
+++ MysteryMachine/Contents/mods/MysteryMachine/media/lua/shared/MysteryMachineOGSNVehicleZoneDefinition.lua
@@ -2,22 +2,22 @@ if VehicleZoneDistribution then
 
 -- Commented lines will work, uncomment them if it seems like they aren't spawning enough
 
--- VehicleZoneDistribution.parkingstall.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.trailerpark.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.bad.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.medium.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.good.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.junkyard.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
--- VehicleZoneDistribution.trafficjamw.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.parkingstall.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.trailerpark.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.bad.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.medium.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.good.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.junkyard.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+-- VehicleZoneDistribution.trafficjamw.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
 
-VehicleZoneDistribution.fire.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.ranger.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.mccoy.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.postal.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.spiffo.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.ambulance.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.radio.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.fossoil.vehicles["Base.VanMysteryMachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.fire.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.ranger.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.mccoy.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.postal.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.spiffo.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.ambulance.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.radio.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.fossoil.vehicles["Base.VanMysterymachine"] = {index = -1, spawnChance = 1};
 
 
 end
