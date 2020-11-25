7ec1c50983f9e1b94d8d923ad5a56a57b9504ebb
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-11-25 02:16:23 -0500

    Orphanage - Good Caffeine - Removes coffee changes until the devs sort out the new bugs

--- ForkMJorphans/Contents/mods/OGSN_orphan_GoodTeaCoffeeVitamins/media/lua/server/Items/PillsZombieLootOGSN.lua
+++ ForkMJorphans/Contents/mods/OGSN_orphan_GoodTeaCoffeeVitamins/media/lua/server/Items/PillsZombieLootOGSN.lua
@@ -7,3 +7,4 @@ table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.03);
 -- Zombie Female
 table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "Base.PillsVitamins");
 table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.03);
+ 
