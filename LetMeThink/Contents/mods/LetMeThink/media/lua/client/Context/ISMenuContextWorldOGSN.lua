df03b87ee55a7402fbcad8973dfd4cfd865cc96e
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-12-16 23:09:48 -0500

    pastes in the replacement function

--- LetMeThink/Contents/mods/LetMeThink/media/lua/client/Context/ISMenuContextWorldOGSN.lua
+++ LetMeThink/Contents/mods/LetMeThink/media/lua/client/Context/ISMenuContextWorldOGSN.lua
@@ -13,9 +13,64 @@ function ISMenuContextWorld.new()
   local self = ISMenuContextWorld_original.new();
   self.createMenu_original = self.createMenu
   function self.createMenu_new( _playerNum, _object, _objects, _x, _y, _test )
-        -- rest goes here
-  end
+    print('In the modded version')
+    local playerObj = getSpecificPlayer(_playerNum);
+    if playerObj:isDead() or playerObj:isAsleep() then return end
+    self.reset(_playerNum);
+    local context;
+    if not _test then
+      context = ISWorldObjectContextMenu.createMenu( _playerNum, _objects, _x, _y ) or ISContextMenu.get( _playerNum, _x, _y );
+      context:setVisible(true);
+    else
+      local test = ISWorldObjectContextMenu.createMenu( _playerNum, _objects, _x, _y, _test );
+      if test then
+        return true;
+      end
+      context = ISContextMenu.get( _playerNum, _x, _y );
+      context:setVisible(false);
+    end
 
+    getCell():setDrag(nil, _playerNum);
+
+    local contextData = self.getContextData(_playerNum);
+
+    contextData.context = context;
+    contextData.playerNum = _playerNum;
+    contextData.player = playerObj;
+    contextData.playerRoom = playerObj:getCurrentSquare():getRoom();
+    contextData.inventory = playerObj:getInventory();
+    contextData.object = _object;
+    contextData.objects = _objects;
+    contextData.test = _test;
+
+    if JoypadState.players[_playerNum + 1] then
+      contextData.isJoypad = true;
+      contextData.playerData = getPlayerData(_playerNum);
+    end
+
+    contextData.objects = self.getAllObjects(contextData);
+
+    if not JoypadState.players[_playerNum + 1] then
+      local wx, wy = ISCoordConversion.ToWorld( _x * getCore():getZoom(contextData.playerNum), _y * getCore():getZoom(contextData.playerNum), contextData.player:getZ() );
+      self.getObjectsSquare( contextData, getCell():getGridSquare(wx, wy, contextData.player:getZ()) );
+    end
+
+    self.printDebug(contextData);
+
+    for index, element in self.elements.indexIterator() do
+      local test = element.createMenu( contextData );
+      if _test and test then
+        return true;
+      end
+    end
+
+    if _test then return false end
+
+    if context.numOptions == 1 then
+      context:setVisible(false);
+    end
+    return context;
+  end
   function self.createMenu( _playerNum, _object, _objects, _x, _y, _test )
     if UIManager.getSpeedControls():getCurrentGameSpeed() ~= 0 then
         self.createMenu_original( _playerNum, _object, _objects, _x, _y, _test )
