6b490548117e0a935f5b1c2745a53898330e9e15
Oh God Spiders No <ohgodspidersno@gmail.com>
2020-12-18 20:25:41 -0500

    New code to determine the final quality of an upgraded spear. Retrieves all variables needed for calculations, but does not yet perform the calculations.

--- TieOnSpearheads/Contents/mods/TieOnSpearheads/media/lua/server/recipecodeTieSpearsOGSN.lua
+++ TieOnSpearheads/Contents/mods/TieOnSpearheads/media/lua/server/recipecodeTieSpearsOGSN.lua
@@ -1,31 +1,53 @@
 Recipe = Recipe or {}
 Recipe.OnCreate = Recipe.OnCreate or {}
 
-
 function Recipe.OnCreate.UpgradeSpear(items, result, player, selectedItem)
-    local conditionMax = 0;
+    -- Get the stats of the original spear, spearhead, and binding
+    local binding, oHP, oMaxHP, oHPpercent, spearheadHP, spearheadMaxHP, spearheadHPpercent;
     for i=0,items:size() - 1 do
-        if items:get(i):getType() == "SpearCrafted" then
-            conditionMax = items:get(i):getCondition()
+        local item = items:get(i);
+        local type = item:getType();
+        -- original spear's condition (checks for spears from MorePoleWeapons, too)
+        if type == "SpearCrafted" or type == "LongSpear" or type == "ShovelSpear" or type == "Shovel2Spear" or type == "SpearShovel" or type == "SpearShovel2" then
+            oHP = item:getCondition();
+            oMaxHP = item:getConditionMax();
+            oHPpercent = oHP/oMaxHP;
+        end
+        -- binding used
+        binding = type if type=="DuctTape" or type=="Twine" or type=="LeatherStrips" else end
+        -- spearhead's stats and condition
+        if instanceof (item, "HandWeapon") and type ~= "SpearCrafted" and type ~= "LongSpear" and type ~= "ShovelSpear" and type ~= "Shovel2Spear" and type ~= "SpearShovel" and type ~= "SpearShovel2" then
+          spearheadHP = item:getCondition();
+          spearheadMaxHP = item:getConditionMax();
+          spearheadHPpercent = spearheadHP/spearheadMaxHP;
+        elseif type == "SharpedStone" then
+          spearheadHPpercent = (ZombRand(7,11)*2)/20
         end
     end
 
-    for i=0,items:size() - 1 do
-        if instanceof (items:get(i), "HandWeapon") and items:get(i):getType() ~= "SpearCrafted" then
-            conditionMax = conditionMax - ((items:get(i):getConditionMax() - items:get(i):getCondition())/2)
-        elseif items:get(i):getType() == "SharpedStone" then
-            conditionMax = conditionMax*2 + ZombRand(-1,2) -- A perfect 5 will become a perfect 10. fuzzed +/-1 by chance.
-        end
-    end
+    -- get final quality modifiers from player stats and chance
+    local skillLevel = player:getPerkLevel(Perks.Woodwork);
+    local skillModHP = skillLevel/5 -- woodworking improves condition, maxing at lvl 5
+    local skillModDMG = (skillLevel%5) -- woodworking lvls 6-10 improve damage, too
+    local chanceDelta = ZombRand(-1,1);
 
-    if conditionMax > result:getConditionMax() then
-        conditionMax = result:getConditionMax();
-    end
-    if conditionMax < 2 then
-        conditionMax = 2;
-    end
+    -- get the product's base stats
+    local rMaxHP = result:getConditionMax();
+    local rMinDmg = result:getMinDamage();
+    local rMaxDmg = result:getMaxDamage();
+    local rName = result:getDisplayName();
 
-    result:setCondition(conditionMax);
+    -- define the final modified stats of the resulting spear
+    local rHP, rMinDmgNew, rMaxDmgNew, rNameNew
+
+    -----------------------------
+    --- do calculations here ----
+    -----------------------------
+
+    result:setCondition(rHP)
+    result:setMinDamage(rMinDmgNew)
+    result:setMaxDamage(rMaxNew)
+    result:setDisplayName(rNameNew)
 end
 
 UpgradeSpear_OnCreate = Recipe.OnCreate.UpgradeSpear
