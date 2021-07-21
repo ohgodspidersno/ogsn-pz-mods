9572823108f8d0ef44a7c768dd5f4d701e19e593
Oh God Spiders No <ohgodspidersno@gmail.com>
2021-07-21 14:34:32 -0400

    LetMeThink 41.51 (including 41.50 legacy support with gamepad fixes)

--- LetMeThink/Contents/mods/LetMeThink/media/lua/client/ISUI/ISDPadWheels.lua
+++ LetMeThink/Contents/mods/LetMeThink/media/lua/client/ISUI/ISDPadWheels.lua
@@ -7,95 +7,95 @@ require "ISBaseObject"
 ISDPadWheels = {}
 
 local function isEquippedOrAttached(playerObj, item)
-	return playerObj:isEquipped(item) or playerObj:isAttachedItem(item)
+  return playerObj:isEquipped(item) or playerObj:isAttachedItem(item)
 end
 
 function ISDPadWheels.onDisplayLeft(joypadData)
-	local playerIndex = joypadData.player
-	local playerObj = getSpecificPlayer(playerIndex)
+  local playerIndex = joypadData.player
+  local playerObj = getSpecificPlayer(playerIndex)
 
-	local menu = getPlayerRadialMenu(playerIndex)
-	menu:clear()
+  local menu = getPlayerRadialMenu(playerIndex)
+  menu:clear()
 
-	local bestLight,bestLightEquipped = nil
-	local inv = playerObj:getInventory():getItems()
-	for i=1,inv:size() do
-		local item = inv:get(i-1)
-		if instanceof(item, "HandWeapon") and item:getCondition() > 0 then
-			menu:addSlice(item:getDisplayName(), item:getTex(), ISDPadWheels.onEquipPrimary, playerObj, item)
-		elseif item:canEmitLight() then
-			if not bestLight then
-				bestLight = item
-			elseif bestLight:getLightStrength() < item:getLightStrength() then
-				bestLight = item
-			end
-			if isEquippedOrAttached(playerObj, item) then
-				if not bestLightEquipped then
-					bestLightEquipped = item
-				elseif bestLightEquipped:getLightStrength() < item:getLightStrength() then
-					bestLightEquipped = item
-				end
-			end
-		end
-	end
+  local bestLight, bestLightEquipped = nil
+  local inv = playerObj:getInventory():getItems()
+  for i = 1, inv:size() do
+    local item = inv:get(i - 1)
+    if instanceof(item, "HandWeapon") and item:getCondition() > 0 then
+      menu:addSlice(item:getDisplayName(), item:getTex(), ISDPadWheels.onEquipPrimary, playerObj, item)
+    elseif item:canEmitLight() then
+      if not bestLight then
+        bestLight = item
+      elseif bestLight:getLightStrength() < item:getLightStrength() then
+        bestLight = item
+      end
+      if isEquippedOrAttached(playerObj, item) then
+        if not bestLightEquipped then
+          bestLightEquipped = item
+        elseif bestLightEquipped:getLightStrength() < item:getLightStrength() then
+          bestLightEquipped = item
+        end
+      end
+    end
+  end
 
-	bestLight = bestLightEquipped or bestLight
-	if bestLight then
-		menu:addSlice(bestLight:getDisplayName(), bestLight:getTex(), ISDPadWheels.onToggleLight, playerObj, bestLight)
-	end
+  bestLight = bestLightEquipped or bestLight
+  if bestLight then
+    menu:addSlice(bestLight:getDisplayName(), bestLight:getTex(), ISDPadWheels.onToggleLight, playerObj, bestLight)
+  end
 
-	menu:setX(getPlayerScreenLeft(playerIndex) + getPlayerScreenWidth(playerIndex) / 2 - menu:getWidth() / 2)
-	menu:setY(getPlayerScreenTop(playerIndex) + getPlayerScreenHeight(playerIndex) / 2 - menu:getHeight() / 2)
-	menu:addToUIManager()
-	menu:setHideWhenButtonReleased(Joypad.DPadLeft)
-	setJoypadFocus(playerIndex, menu)
-	playerObj:setJoypadIgnoreAimUntilCentered(true)
+  menu:setX(getPlayerScreenLeft(playerIndex) + getPlayerScreenWidth(playerIndex) / 2 - menu:getWidth() / 2)
+  menu:setY(getPlayerScreenTop(playerIndex) + getPlayerScreenHeight(playerIndex) / 2 - menu:getHeight() / 2)
+  menu:addToUIManager()
+  menu:setHideWhenButtonReleased(Joypad.DPadLeft)
+  setJoypadFocus(playerIndex, menu)
+  playerObj:setJoypadIgnoreAimUntilCentered(true)
 end
 
 function ISDPadWheels.onDisplayRight(joypadData)
-	local playerIndex = joypadData.player
-	local playerObj = getSpecificPlayer(playerIndex)
+  local playerIndex = joypadData.player
+  local playerObj = getSpecificPlayer(playerIndex)
 
-	local menu = getPlayerRadialMenu(playerIndex)
-	menu:clear()
+  local menu = getPlayerRadialMenu(playerIndex)
+  menu:clear()
 
-	menu:setX(getPlayerScreenLeft(playerIndex) + getPlayerScreenWidth(playerIndex) / 2 - menu:getWidth() / 2)
-	menu:setY(getPlayerScreenTop(playerIndex) + getPlayerScreenHeight(playerIndex) / 2 - menu:getHeight() / 2)
-	menu:addToUIManager()
-	menu:setHideWhenButtonReleased(Joypad.DPadRight)
-	setJoypadFocus(playerIndex, menu)
-	playerObj:setJoypadIgnoreAimUntilCentered(true)
+  menu:setX(getPlayerScreenLeft(playerIndex) + getPlayerScreenWidth(playerIndex) / 2 - menu:getWidth() / 2)
+  menu:setY(getPlayerScreenTop(playerIndex) + getPlayerScreenHeight(playerIndex) / 2 - menu:getHeight() / 2)
+  menu:addToUIManager()
+  menu:setHideWhenButtonReleased(Joypad.DPadRight)
+  setJoypadFocus(playerIndex, menu)
+  playerObj:setJoypadIgnoreAimUntilCentered(true)
 end
 
 function ISDPadWheels.onDisplayUp(joypadData)
-	ISVehicleMenu.showRadialMenu(getSpecificPlayer(joypadData.player))
+  ISVehicleMenu.showRadialMenu(getSpecificPlayer(joypadData.player))
 end
 
 function ISDPadWheels.onDisplayDown(joypadData)
-	local playerIndex = joypadData.player
-	local playerObj = getSpecificPlayer(playerIndex)
+  local playerIndex = joypadData.player
+  local playerObj = getSpecificPlayer(playerIndex)
 
-	local erm = ISEmoteRadialMenu:new(playerObj)
-	erm:fillMenu()
+  local erm = ISEmoteRadialMenu:new(playerObj)
+  erm:fillMenu()
 end
 
 function ISDPadWheels.onEquipPrimary(playerObj, item)
-	-- FIXME: ISInventoryPaneContextMenu checks for injured hands (code is broken though)
-	if item and item == playerObj:getPrimaryHandItem() then
-		ISTimedActionQueue.add(ISUnequipAction:new(playerObj, playerObj:getPrimaryHandItem(), 50));
-	else
-		ISTimedActionQueue.add(ISEquipWeaponAction:new(playerObj, item, 50, true, item:isTwoHandWeapon()));
-	end
+  -- FIXME: ISInventoryPaneContextMenu checks for injured hands (code is broken though)
+  if item and item == playerObj:getPrimaryHandItem() then
+    ISTimedActionQueue.add(ISUnequipAction:new(playerObj, playerObj:getPrimaryHandItem(), 50));
+  else
+    ISTimedActionQueue.add(ISEquipWeaponAction:new(playerObj, item, 50, true, item:isTwoHandWeapon()));
+  end
 end
 
 function ISDPadWheels.onToggleLight(playerObj, item)
-	if not isEquippedOrAttached(playerObj, item) then
-		ISTimedActionQueue.add(ISEquipWeaponAction:new(playerObj, item, 50, false))
-	else
-		item:setActivated(not item:isActivated())
-	end
+  if not isEquippedOrAttached(playerObj, item) then
+    ISTimedActionQueue.add(ISEquipWeaponAction:new(playerObj, item, 50, false))
+  else
+    item:setActivated(not item:isActivated())
+  end
 end
 
 function ISDPadWheels.onShout(playerObj)
-	playerObj:Callout()
+  playerObj:Callout()
 end
